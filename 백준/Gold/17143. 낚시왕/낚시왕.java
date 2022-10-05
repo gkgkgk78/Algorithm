6437721a481ba3fc1c3e32c7d382e37a99c14f8a
gkgkgk78 <78834028+gkgkgk78@users.noreply.github.com>
2022-10-05 21:32:08 +0900

    [Gold I] Title: 낚시왕, Time: 1156 ms, Memory: 35200 KB -BaekjoonHub

--- 백준/Gold/17143. 낚시왕/낚시왕.java
+++ 백준/Gold/17143. 낚시왕/낚시왕.java
@@ -1,29 +1,26 @@
+import java.awt.List;
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.HashMap;
+import java.util.Stack;
 
 public class Main {
-
-	static int graph[][];
-	static int col;
-	static int c, r;
-	static ArrayList<go> hi;
+	static int r, c, m;
 	static int result = 0;
-	static int visit[][];
 
 	static class go {
-		int x, y;
-		int velocity, dir, size;
 
-		public go(int x, int y, int velocity, int dir, int size) {
+		int x, y, velo, dir, size;
 
+		public go(int x, int y, int velo, int dir, int size) {
+			super();
 			this.x = x;
 			this.y = y;
-			this.velocity = velocity;
+			this.velo = velo;
 			this.dir = dir;
 			this.size = size;
 		}
@@ -34,34 +31,43 @@ public class Main {
 			this.y = y;
 		}
 
+		public go(int x, int y, int velo, int dir) {
+			super();
+			this.x = x;
+			this.y = y;
+			this.velo = velo;
+			this.dir = dir;
+		}
+
 	}
 
+	static ArrayList<go> shark;
+
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		
 
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		int m;
-		StringTokenizer s = new StringTokenizer(in.readLine());
+
+		StringTokenizer s = new StringTokenizer(in.readLine(), " ");
 
 		r = Integer.parseInt(s.nextToken());
 		c = Integer.parseInt(s.nextToken());
 		m = Integer.parseInt(s.nextToken());
-		graph = new int[r + 1][c + 1];
-		int a1, a2, a3, a4, a5;
-
-		hi = new ArrayList<>();
-		col = 0;
+		shark = new ArrayList<>();
 		for (int i = 0; i < m; i++) {
-			s = new StringTokenizer(in.readLine());
-			a1 = Integer.parseInt(s.nextToken());
-			a2 = Integer.parseInt(s.nextToken());
-			a3 = Integer.parseInt(s.nextToken());
-			a4 = Integer.parseInt(s.nextToken());
-			a5 = Integer.parseInt(s.nextToken());
-			graph[a1][a2] = 1;
-			hi.add(new go(a1, a2, a3, a4, a5));
+
+			s = new StringTokenizer(in.readLine(), " ");
+			int x, y, vel, dir, size;
+			x = Integer.parseInt(s.nextToken());
+			x--;
+			y = Integer.parseInt(s.nextToken());
+			y--;
+			vel = Integer.parseInt(s.nextToken());
+			dir = Integer.parseInt(s.nextToken());
+			size = Integer.parseInt(s.nextToken());
+			shark.add(new go(x, y, vel, dir, size));
+
 		}
+
 		game();
 		System.out.println(result);
 
@@ -70,128 +76,233 @@ public class Main {
 	private static void game() {
 		// TODO Auto-generated method stub
 
-		while (true) {
-			col++;
-			if (col == c + 1) {
-				return;
-			}
-//			for (int i = 1; i <= r; i++) {
-//				System.out.println(Arrays.toString(graph[i]));
-//			}
-			//System.out.println();
+		for (int i = 0; i < c; i++) {
 
-			int ch = col;
-			// 낚시왕이 가장 가까운 상어를 잡음
-			for (int i = 1; i <= r; i++) {
-				if (graph[i][ch] ==1) {
-					graph[i][ch] = 0;
-					for (int i1 = hi.size() - 1; i1 >= 0; i1--) {
-						go p=hi.get(i1);
-						if (p.x == i && p.y == ch) {
-							result += p.size;
-							hi.remove(i1);
-							break;
-						}
-					}
-					break;
-				}
+			// 낚시왕과 가장 가까운 상어를 한마리 잡음
+			// System.out.println(i);
+			eat(i);
 
-			}
-			ArrayList<go> temp = new ArrayList<>();
-			// 낚시왕이 있는 열에 있는 상에중 가장 가까운 상어를 잡음
-			int ct = 0;
-			visit=new int[r+1][c+1];
-			for (go tt : hi) {
-				// 상어들 이동을 하도록 함.
-				move(temp, tt.x, tt.y, tt.dir, tt.size, tt.velocity, ct);
-				ct++;
-			}
-//			System.out.println(col);
-//			for (go tt : temp) {
-//
-//				System.out.println(tt.x + " " + tt.y + " " + tt.size);
-//
-//			}
-			for (go tt : temp) {
-				int a1 = tt.x;
-				int a2 = tt.y;
-				int uu = Integer.MIN_VALUE;
-				
-				for (int i1 = hi.size() - 1; i1 >= 0; i1--) {
-					go ye = hi.get(i1);
-					if (ye.x == a1 && ye.y == a2) {
-						if(ye.size>uu)
-							uu=ye.size;
-					}
-				}
-	
-				for (int i1 = hi.size() - 1; i1 >= 0; i1--) {
-					go ye = hi.get(i1);
-					if (ye.x == a1 && ye.y == a2 && ye.size < uu) {
-						hi.remove(i1);
-					}
-				}
+			// 상어를 이동시킴
 
-				graph[a1][a2] = 1;
-			}
-//			System.out.println(col);
-//			for (go tt : hi) {
-//				// 상어들 이동을 하도록 함.
-//				System.out.println(tt.x + " " + tt.y + " " + tt.size + " " + tt.dir);
-//
-//			}
-//			System.out.println();
+			move();
+			// System.out.println();
+			// 상어가 중복된 곳에 존재를 할시 가장 큰 상어를 제외하고 제거
 
 		}
+
 	}
 
-	private static void move(ArrayList<go> temp, int x, int y, int dir, int size, int velocity, int ct) {
+	private static void move() {
 		// TODO Auto-generated method stub
-		int u = velocity;
-		int tx = x;
-		int ty = y;
-		int dir1 = dir;
-		if(visit[x][y]==0)
-			graph[x][y] = 0;
-		
-		while (u > 0) {
-			if (dir1 == 1) {
-				x -= 1;
-				if (x == 0) {
-					x += 2;
-					dir1 = 2;
+
+		int graph[][] = new int[r][c];
+		int k1 = 0;
+		ArrayList<go> temp = new ArrayList<>();
+		int dx[] = { 0, -1, 1, 0, 0 };
+		int dy[] = { 0, 0, 0, 1, -1 };
+		for (go t : shark) {
+
+			// 방향에 따라 움직여야 함
+
+			// d가 1인 경우는 위, 2인 경우는 아래, 3인 경우는 오른쪽, 4인 경우는 왼쪽
+			// System.out.println(t.x + " " + t.y + " " + t.dir);
+			if (t.dir == 1) {
+				int zx = t.x;
+				int zy = t.y;
+				int z1 = t.velo;
+				
+
+				while (z1 > 0) {
+
+					int tem = dx[t.dir] * z1;
+
+					if (tem + zx < 0) {
+						int aa=Math.abs(zx-0);
+						z1-=aa;
+						zx=0;
+						t.dir=2;
+
+					} else if (tem + zx >= r) {
+						int aa=Math.abs(r-1-zx);
+						z1-=aa;
+						zx=r-1;
+						t.dir=1;
+						
+					}
+					else
+					{
+						zx=zx+tem;
+						z1=0;
+					}
+
+
 				}
-			} else if (dir1 == 2) {
-				x += 1;
-				if (x == r + 1) {
-					x -= 2;
-					dir1 = 1;
+				t.x = zx;
+				t.y = zy;
+
+			} else if (t.dir == 2) {
+				int zx = t.x;
+				int zy = t.y;
+				int z1 = t.velo;
+				while (z1 > 0) {
+
+					int tem = dx[t.dir] * z1;
+
+					if (tem + zx < 0) {
+						int aa=Math.abs(zx-0);
+						z1=z1-aa;
+						zx=0;
+						t.dir=2;
+
+					} else if (tem + zx >= r) {
+						int aa=Math.abs(r-1-zx);
+						z1=z1-aa;
+						zx=r-1;
+						t.dir=1;
+					}
+					else
+					{
+						zx=zx+tem;
+						z1=0;
+					}
+
+
 				}
-			} else if (dir1 == 3) {
-				y += 1;
-				if (y == c + 1) {
-					y -= 2;
-					dir1 = 4;
+				t.x = zx;
+				t.y = zy;
+
+			} else if (t.dir == 3) {
+				// 오른쪽
+				int zx = t.x;
+				int zy = t.y;
+				int z1 = t.velo;
+				while (z1 > 0) {
+
+					int tem = dy[t.dir] * z1;
+
+					if (tem + zy < 0) {
+						int aa=Math.abs(zy-0);
+						z1-=aa;
+						zy=0;
+						t.dir=3;
+
+					} else if (tem + zy >= c) {
+						int aa=Math.abs(c-1-zy);
+						z1-=aa;
+						zy=c-1;
+						t.dir=4;
+					}
+					else
+					{
+						zy=zy+tem;
+						z1=0;
+					}
+
 				}
-			} else if (dir1 == 4) {
-				y -= 1;
-				if (y == 0) {
-					y += 2;
-					dir1 = 3;
+				t.x = zx;
+				t.y = zy;
+
+			} else if (t.dir == 4) {
+				// 왼쪽
+				int zx = t.x;
+				int zy = t.y;
+				int z1 = t.velo;
+
+				while (z1 > 0) {
+
+					int tem = dy[t.dir] * z1;
+
+					if (tem + zy < 0) {
+						int aa=Math.abs(zy-0);
+						z1-=aa;
+						zy=0;
+						t.dir=3;
+
+					} else if (tem + zy >= c) {
+						int aa=Math.abs(c-1-zy);
+						z1-=aa;
+						zy=c-1;
+						t.dir=4;
+					}
+					else
+					{
+						zy=zy+tem;
+						z1=0;
+					}
+
+
+				}
+				
+				t.x = zx;
+				t.y = zy;
+
+			}
+			// System.out.println(t.x+" "+t.y);
+			graph[t.x][t.y] += 1;
+			if (graph[t.x][t.y] == 2) {
+				temp.add(new go(t.x, t.y));
+			}
+
+		}
+//		for(int i=0;i<r;i++)
+//			System.out.println(Arrays.toString(graph[i]));
+//		System.out.println();
+//		
+
+		for (go g : temp) {
+			// System.out.println("아하");
+			// 중복된 상어들의 좌표만 존재를 함
+			int x1 = g.x;
+			int y1 = g.y;
+			int size = Integer.MIN_VALUE;
+			ArrayList<Integer> op = new ArrayList<>();
+			for (int u = 0; u < shark.size(); u++) {
+				go g1 = shark.get(u);
+				if (g1.x == x1 && g1.y == y1) {
+					op.add(u);
+					size = Math.max(g1.size, size);
 				}
 			}
-			u--;
+
+			// 이제 op에 들은거 삭제를 해야함
+			for (int u = shark.size() - 1; u >= 0; u--) {
+				go g1 = shark.get(u);
+				if (g1.x == x1 && g1.y == y1) {
+					if (g1.size != size) {
+						shark.remove(u);
+					}
+				}
+			}
 		}
-		graph[x][y]=1;
-		visit[x][y]+=1;
-		if (visit[x][y]==2) {
-			visit[x][y]+=1;
-			temp.add(new go(x, y));
+
+	}
+
+	private static void eat(int now) {
+		// TODO Auto-generated method stub
+
+		int get = -1;
+		int index = Integer.MAX_VALUE;
+		for (int i = 0; i < c; i++) {
+
+			for (int j = 0; j < shark.size(); j++) {
+				go l = shark.get(j);
+				if (l.y == now) {
+					if (l.x < index) {
+						index = l.x;
+						get = j;
+					}
+				}
+
+			}
+
 		}
-		go e=hi.get(ct);
-		e.x = x;
-		e.y = y;
-		e.dir = dir1;
+
+		if (get != -1) {
+			go t = shark.get(get);
+			result += t.size;
+			// System.out.println(t.size);
+			shark.remove(t);
+		}
 
 	}
 
