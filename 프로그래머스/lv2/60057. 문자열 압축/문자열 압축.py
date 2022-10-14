bad86819ef038fea1a8bfc4a44ead842b0d8345b
gkgkgk78 <78834028+gkgkgk78@users.noreply.github.com>
2022-10-14 22:59:49 +0900

    [level 2] Title: 문자열 압축, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.py
+++ 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.py
@@ -1,40 +1,31 @@
 def solution(s):
-    answer = int(1e9)
-    if len(s)==1:
-        return 1
+    answer = len(s)
+    ans=""
+    #문자열을 길이 단위로 해서 나누도록 하자
     for i in range(1, len(s) // 2 + 1):
         last = ""
         temp = []
         j = 0
-        if i==2:
-            j=0
-        while j < len(s)+1:
-            if len(temp) == 0:
-                zz = s[j:j + i]
-                temp.append(zz)
+        t = []
+        while (j < len(s)+1):
+            t = s[j:j + i]
+            if (len(temp) == 0):
+                temp.append(t)
             else:
-                next_val = s[j:j + i]
-                if (temp[-1] == next_val):
-                    temp.append(next_val)
+                if (t == temp[-1]):
+                    temp.append(t)
                 else:
-
                     if (len(temp) == 1):
-                        last = last + temp[0]
-                        temp = []
-                        temp.append(next_val)
+                        # temp.append(t[-1])
+                        last += temp[-1]
                     else:
-                        front_size = len(temp)
-                        #if front_size>0:
-                        last = last + str(front_size) + temp[-1]
-                        # else:
-                        #     last=last+temp[0]
-                        temp = []
-                        temp.append(next_val)
-            j = j + i
-        if(len(temp)>0):
-            last+=temp[-1]
+                        last = last + str(len(temp)) + temp[-1]
+                    temp=[]
+                    temp.append(t)
+            j=j+i
+        if (len(temp)>0):
+            last += temp[-1]
 
-        #print(last,len(last))
-            
         answer=min(answer,len(last))
+
     return answer
