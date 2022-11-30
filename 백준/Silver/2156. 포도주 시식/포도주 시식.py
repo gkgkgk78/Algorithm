f8791291e61acac695d7c51ceed2f64ff3c329d3
gkgkgk78 <78834028+gkgkgk78@users.noreply.github.com>
2022-11-30 20:50:23 +0900

    [Silver I] Title: 포도주 시식, Time: 132 ms, Memory: 115412 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.py
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -1,26 +1,7 @@
-import sys, copy, heapq
-import heapq, math
-from queue import PriorityQueue
-from itertools import permutations, combinations, product
-from collections import deque
-from itertools import product
-
-#sys.setrecursionlimit(10 ** 5)
-#a=list(product(i,repeat=len(powers)))
-#b=list(product(*a)) #리스트 안에 있는 원소들끼리 조합
-
-
-from itertools import combinations_with_replacement as cwr
-from collections import Counter
+import sys
 input = sys.stdin.readline
-
-
-
 #정수 x가 줭졌을때, 연산 4개를 적절히 사용해서 1을 만들려고 한다.
 #연산을 사용하는 횟수의 최솟ㄱ밧을 출력하시오!
-
-
-
 x=int(input())
 
 d=[0 for _ in range(x+1)]
