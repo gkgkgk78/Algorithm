a9292f0be52ef9afa5cf7468847ccde07e801f7d
gkgkgk78 <78834028+gkgkgk78@users.noreply.github.com>
2022-12-03 01:05:31 +0900

    [Silver III] Title: 이친수, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+
+
+
+n=int(input().rstrip())
+
+dp=[0 for _ in range(91)]
+dp[1]=1
+dp[2]=1
+if(n>=3):
+    for i in range(3,n+1):
+        dp[i]=dp[i-1]+(dp[i-2])
+print(dp[n])
