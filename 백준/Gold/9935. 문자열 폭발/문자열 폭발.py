1a96265b74a8d4d228b61d6ee20cb76da83fad96
gkgkgk78 <78834028+gkgkgk78@users.noreply.github.com>
2022-12-15 20:55:08 +0900

    [Gold IV] Title: 문자열 폭발, Time: 740 ms, Memory: 50724 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -1,33 +1,31 @@
-import sys, copy
-import heapq
-from itertools import combinations
+import sys
 from collections import deque
+input=sys.stdin.readline
 
-input = sys.stdin.readline
-import math
-t=input().rstrip()
-g=input().rstrip()
-hi=g[-1]
-d=[]
-for i in range(len(t)):
-    if t[i]!=hi:
-        d.append(t[i])
-    else:
-        fy=0
-        d.append(t[i])
-        go = len(d)-1
-        if len(d)>0:
-            for j in range(len(g)-1,-1,-1):
-                if g[j]==d[go]:
-                    go-=1
-                elif g[j]!=d[go]:
-                    fy=1
+
+e=list(input().rstrip())
+test=list(input().rstrip())
+
+last=test[-1]
+leng=len(test)
+stack=[]
+for i in e:
+    stack.append(i)
+    if i==last and len(stack)>=leng:
+        la=len(stack)
+        tt=0
+        check=0
+        if la-leng>=0:
+            for k in range(la-leng,la):
+                if test[tt]!=stack[k]:
+                    check=1
                     break
-            if fy==0:
-                for k in range(len(g)):
-                    d.pop()
-if d:
-    for i in d:
-        print(i,end="")
+                tt += 1
+            if check==0:
+                for k in range(len(test)):
+                    stack.pop()
+
+if stack:
+    print("".join(stack))
 else:
     print("FRULA")
