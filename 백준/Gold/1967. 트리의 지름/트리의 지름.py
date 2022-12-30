7a44d93f54139dbdd61236c6e44a0d6291f2a755
gkgkgk78 <78834028+gkgkgk78@users.noreply.github.com>
2022-12-30 21:19:49 +0900

    [Gold IV] Title: 트리의 지름, Time: 72 ms, Memory: 35348 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.py
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.py
@@ -1,36 +1,34 @@
-import sys, copy
-import heapq,math
-from itertools import combinations
-from collections import deque
-sys.setrecursionlimit(10**9)
+import sys
+sys.setrecursionlimit(10**5)
+input=sys.stdin.readline
+n=int(input().rstrip())
+ans=-sys.maxsize
+tree=[[]for _ in range(n+1)]
 
-input = sys.stdin.readline
-
-a=int(input().rstrip())
-
-graph=[[] for _ in range(a+1)]
-
-
-for i in range(a-1):
+for _ in range(n-1):
     a1,a2,a3=map(int,input().split())
-    graph[a1].append([a2,a3])
-    graph[a2].append([a1, a3])
-total=-1
-find=0
-l_find=0
-l_sumz=-1
-visit=[-1]*10001
-def dfs(start,weight):
-    global total,sumz
-    for i in graph[start]:
-        a,b=i
-        if visit[a]==-1:
-            visit[a]=weight+b
-            dfs(a,weight+b)
-visit[1]=0
-dfs(1,0)
-start=visit.index(max(visit))
-visit=[-1]*10001
-visit[start]=0
-dfs(start,0)
-print(max(visit))
+    tree[a1].append((a2,a3))
+
+answer=0
+def dfs(vertex,value):
+    global answer
+    if len(tree[vertex])==0:
+        return value
+
+    left=0
+    right=0
+    jason=[]
+    for i in range(len(tree[vertex])):
+        down_left=tree[vertex][i]
+        left=dfs(down_left[0],down_left[1]+value)
+        jason.append(left)
+
+    if len(tree[vertex])>=2:
+        jason.sort()
+        answer=max(answer,jason[-1]+jason[-2]-value*2)
+    else:
+        answer = max(answer, jason[0] - value )
+    return(max(jason))
+
+ans=dfs(1,0)
+print(answer)
