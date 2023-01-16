bc8e422483b0103538193e1e947f452ccf34499e
gkgkgk78 <78834028+gkgkgk78@users.noreply.github.com>
2023-01-16 20:21:14 +0900

    [Silver II] Title: 연속합, Time: 96 ms, Memory: 38432 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.py
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -1,7 +1,11 @@
-n=int(input())
-a=list(map(int,input().split()) )
+import sys
+input = sys.stdin.readline
 
-for i in range(1,n):
-    a[i]=max(a[i],a[i-1]+a[i])
 
-print(max(a))
+
+n = int(input())
+a = list(map(int, input().split()))
+sum = [a[0]]
+for i in range(len(a) - 1):
+    sum.append(max(sum[i] + a[i + 1], a[i + 1]))
+print(max(sum))
