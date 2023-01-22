4b743687b2c716bebf816ee1c0fddbc0c51f2eb6
gkgkgk78 <78834028+gkgkgk78@users.noreply.github.com>
2023-01-23 00:37:54 +0900

    [Gold III] Title: 색종이 - 3, Time: 292 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Gold/2571. 색종이 － 3/색종이 － 3.py
+++ 백준/Gold/2571. 색종이 － 3/색종이 － 3.py
@@ -1,45 +1,54 @@
+
 import sys
 input = sys.stdin.readline
+
+
 graph=[[0]*100 for _ in range(100)]
+
+
 n=int(input().rstrip())
 for _ in range(n):
     a1,a2=map(int,input().split())
     a1-=1
     a2-=1
-    if a1==14:
-        a1=14
     for i in range(a1,a1+10):
         for j in range(a2,a2+10):
-            graph[i][j]+=1
+            graph[i][j]=1
 
 answer=-sys.maxsize
 
-def check(x,y,width,height):
-    #가로축 확인
-    temp=0
-    for i in range(x,x+width+1):
-        for j in range(y,y+height+1):
-            if graph[i][j]==0:
-                return -1
-            temp+=1
-    return temp
 
+def check(x,y,width):
+
+    #현재 시작 점에서의 세로의 누적합을 의미를 함
+    now_min=graph[x][y]
+
+
+    for i in range(y,y+width+1):
+        now_min=min(now_min,graph[x][i])
+    if now_min*width==112:
+        now_min=now_min
+
+
+    return now_min*(width+1)
 
 
 def go(x,y):
     now=-sys.maxsize
-    for i in range(0,100):
-        if x+i>=100:
+    for j in range(0,101):
+        if y+j>=100:
             break
-        for j in range(0,100):
-           if j + y >= 100:
-               break
-           temp=check(x,y,i,j)
-           if (temp)>=0:
-               now=max(now,temp)
-
+        if graph[x][y+j]==0:
+            break
+        temp=check(x,y,j)
+        now=max(temp,now)
     return now
 
+
+for i in range(99):
+    for j in range(100):
+        if graph[i][j]!=0 and graph[i+1][j]!=0:
+            graph[i+1][j]=graph[i][j]+1
 
 for i in range(100):
     for j in range(100):
