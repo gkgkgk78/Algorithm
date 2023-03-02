69036c119971d0f28ea96cf1a24cc7c097e00b0d
gkgkgk78 <78834028+gkgkgk78@users.noreply.github.com>
2023-03-02 23:01:52 +0900

    [Gold V] Title: MooTube (Silver), Time: 3340 ms, Memory: 34136 KB -BaekjoonHub

--- 백준/Gold/15591. MooTube （Silver）/MooTube （Silver）.py
+++ 백준/Gold/15591. MooTube （Silver）/MooTube （Silver）.py
@@ -1,10 +1,7 @@
 import sys
 from collections import deque
 input = sys.stdin.readline
-
 n,q=map(int,input().split())
-
-total=[[sys.maxsize]*(n+1) for _ in range(n+1)]
 graph=[[] for _ in range(n+1)]
 
 for _ in range(n-1):
@@ -13,30 +10,24 @@ for _ in range(n-1):
     graph[a2].append((a1, a3))
 
 #이제 탐색을 진행 해보도록 하자
-
-
-def bfs(vertex):
-    global total
+def bfs(vertex,l,graph):
     q=deque()
     q.append(vertex)
     visit=[0]*(n+1)
     visit[vertex]=1
-    aa=total
+
+    tt=0
     while q:
         now=q.popleft()
         for i in graph[now]:
             a1,a2=i
-            if visit[a1]==0:
-                total[vertex][a1]=min(a2,total[vertex][now])
+            if visit[a1]==0 and a2>=l:
                 visit[a1]=1
                 q.append(a1)
-
+                tt+=1
+    return tt
 
 for l in range(q):
     a1,a2=map(int,input().split())
-    bfs(a2)
-    temp=0
-    for l in total[a2]:
-        if l>=a1 and l !=sys.maxsize:
-            temp+=1
-    print(temp)
+    e=bfs(a2,a1,graph)
+    print(e)
