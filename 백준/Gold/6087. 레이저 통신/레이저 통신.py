a9ea04423d75d6e4852feb8a3cccec3a44354d6c
gkgkgk78 <78834028+gkgkgk78@users.noreply.github.com>
2023-03-26 15:20:43 +0900

    [Gold III] Title: 레이저 통신, Time: 80 ms, Memory: 34244 KB -BaekjoonHub

--- 백준/Gold/6087. 레이저 통신/레이저 통신.py
+++ 백준/Gold/6087. 레이저 통신/레이저 통신.py
@@ -9,7 +9,7 @@ start = []
 for i in range(n):
     e = list(map(str, input().rstrip()))
     for j in range(m):
-        if e[j].lower() == "c":
+        if e[j] == "C":
             start.append((i, j))
     graph.append(e)
 visit = [[[sys.maxsize] * 4 for _ in range(m)] for _ in range(n)]  # 들어 올때 빛의 방향을 의미를 함
@@ -32,32 +32,32 @@ def check_dir(dir):
 def bfs(x, y):
     q = deque()
     for i in range(4):
-        q.append((0, x, y, i))
+        q.append((x, y, i))
+        visit[x][y][i] = 0
     dx = [-1, 0, 1, 0]
     dy = [0, 1, 0, -1]
-    while (len(q) > 0):
-        count, x1, y1, dir = q.popleft()
+
+    while q:
+        x1, y1, dir = q.popleft()
         zx = x1 + dx[dir]
         zy = y1 + dy[dir]
-
+        now = visit[x1][y1][dir]
         if 0 <= zx < n and 0 <= zy < m:
-            if graph[zx][zy] == "*":
-                continue
-            if visit[zx][zy][dir] <= count:  # 그전에 방문했다면 넘어감
-                continue
-            visit[zx][zy][dir] = count
-            # 여기에선 세가지 방향으로 갈수 있을 것이다
-            # 거울을 사용하지 않고 그냥 가는 경우
-            q.appendleft((count, zx, zy, dir))
-            # 거울을 사용하는 경우
-            temp = check_dir(dir)
-            for a1 in temp:
-                if visit[zx][zy][a1] <= count + 1:
-                    continue
-                visit[zx][zy][a1] = count + 1
-                q.appendleft((count + 1, zx, zy, a1))
-            if graph[zx][zy] == "C":
-                visit[zx][zy][dir] = min(visit[zx][zy][dir], count + 1)
+            if graph[zx][zy] != "*" and visit[zx][zy][dir] > now:
+                if zx==lastx and zy==lasty :
+                    visit[zx][zy][dir] = now
+                else:
+                    visit[zx][zy][dir] = now
+                    # 여기에선 세가지 방향으로 갈수 있을 것이다
+                    # 거울을 사용하지 않고 그냥 가는 경우
+                    q.appendleft((zx, zy, dir))
+                    # 거울을 사용하는 경우
+                    temp = check_dir(dir)
+                    for a1 in temp:
+                        if visit[zx][zy][a1] <= now + 1:
+                            continue
+                        visit[zx][zy][a1] = now + 1
+                        q.append((zx, zy, a1))
 
 
 bfs(startx, starty)
