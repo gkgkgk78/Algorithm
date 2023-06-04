f232384e91d7d9e6690460143b36d50bfd4b9631
gkgkgk78 <78834028+gkgkgk78@users.noreply.github.com>
2023-06-04 12:13:03 +0900

    [Gold IV] Title: 운동, Time: 780 ms, Memory: 86400 KB -BaekjoonHub

--- 백준/Gold/1956. 운동/운동.py
+++ 백준/Gold/1956. 운동/운동.py
@@ -1,25 +1,33 @@
 import sys
-
+import heapq
 input = sys.stdin.readline
 
 v, e = map(int, input().split())
-graph = [[sys.maxsize] * v for _ in range(v)]
+graph=[[]for _ in  range(v+1)]
+dist = [[sys.maxsize] * v for _ in range(v)]
+q=[]
 for _ in range(e):
     a1, a2, a3 = map(int, input().split())
     a1 -= 1
     a2 -= 1
-    graph[a1][a2] = a3
+    graph[a1].append((a2,a3))
+    dist[a1][a2]=a3
+    heapq.heappush(q,(a3,a1,a2))
 
-for k in range(v):
-    for i in range(v):
-        for j in range(v):
-            graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
 
-answer = sys.maxsize
-for i in range(v):
-    answer = min(answer, graph[i][i])
+while q:
+    a1,a2,a3=heapq.heappop(q)
+    if a2==a3:
+        print(a1)
+        exit(0)
 
-if answer == sys.maxsize:
-    print(-1)
-else:
-    print(answer)
+    if dist[a2][a3]<a1:
+        continue
+
+
+    for t1,t2 in graph[a3]:
+        if a1+t2<dist[a2][t1]:
+            dist[a2][t1]=a1+t2
+            heapq.heappush(q,(a1+t2,a2,t1))
+
+print(-1)
