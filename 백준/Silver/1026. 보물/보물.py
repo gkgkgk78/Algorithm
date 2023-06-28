26d14ba24b286fed18ad20fdd332ff78185c0ca5
gkgkgk78 <78834028+gkgkgk78@users.noreply.github.com>
2023-06-28 14:41:32 +0900

    [Silver IV] Title: 보물, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+
+n=int(input().rstrip())
+a=list(map(int,input().split()))
+b=list(map(int,input().split()))
+
+total=0
+a.sort()
+b.sort(reverse=True)
+
+for i in range(n):
+    total+=(a[i]*b[i])
+print(total)
