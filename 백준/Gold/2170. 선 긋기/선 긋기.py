7b05ea0398f5c18e9feb21ec3ce21f3f5d0c4459
gkgkgk78 <78834028+gkgkgk78@users.noreply.github.com>
2023-06-29 22:59:22 +0900

    [Gold V] Title: 선 긋기, Time: 3956 ms, Memory: 207240 KB -BaekjoonHub

---
+++ 백준/Gold/2170. 선 긋기/선 긋기.py
@@ -0,0 +1,31 @@
+import sys
+
+input = sys.stdin.readline
+
+n=int(input().rstrip())
+e=[]
+for _ in range(n):
+    a1,a2=map(int,input().split())
+    e.append([a1,a2])
+e=sorted(e,key=lambda x:x[0])
+
+
+ans=0
+left=e[0][0]
+right=e[0][1]
+ans+=(right-left)
+
+for l in range(1,n):
+    a1,a2=e[l]
+    if a1>=left and a2<=right:
+        continue
+    if a1>right:
+        left=a1
+        right=a2
+        ans += (right - left)
+        continue
+    if a2>=right:
+        ans+=(a2-right)
+        right=a2
+
+print(ans)
