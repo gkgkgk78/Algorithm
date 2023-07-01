2bc8c9ed2a80039c0e7f7abf343cfd8ddbb1ddfd
gkgkgk78 <78834028+gkgkgk78@users.noreply.github.com>
2023-07-01 14:42:15 +0900

    [Silver V] Title: 칠무해, Time: 4728 ms, Memory: 411720 KB -BaekjoonHub

--- 백준/Silver/14729. 칠무해/칠무해.py
+++ 백준/Silver/14729. 칠무해/칠무해.py
@@ -1,12 +1,12 @@
 import sys
-
+import heapq
 
 input = sys.stdin.readline
 n=int(input().rstrip())
 e=[]
 for _ in range(n):
-    e.append(float(input().rstrip()))
-e.sort()
+    heapq.heappush(e,float(input().rstrip()))
+
 
 for i in range(7):
-    print("{:.3f}".format(e[i]))
+    print("{:.3f}".format(heapq.heappop(e)))
