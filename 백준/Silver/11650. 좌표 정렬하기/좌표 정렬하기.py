6ff60af79ff7978f32adc9e21714d64dad0bc1cc
gkgkgk78 <78834028+gkgkgk78@users.noreply.github.com>
2023-07-02 17:47:23 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 396 ms, Memory: 55288 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -0,0 +1,15 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+n=int(input().rstrip())
+e=[]
+for _ in range(n):
+    a1,a2=map(int,input().split())
+    e.append([a1,a2])
+
+e=sorted(e,key=lambda x:(x[0],x[1]))
+
+for a1,a2 in e:
+    print(a1,a2)
