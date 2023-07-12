98392739d14bfd2dcc1de94317496fc0f8eb02c6
gkgkgk78 <78834028+gkgkgk78@users.noreply.github.com>
2023-07-12 14:21:03 +0900

    [Gold II] Title: 불켜기, Time: 160 ms, Memory: 50324 KB -BaekjoonHub

--- 백준/Gold/11967. 불켜기/불켜기.py
+++ 백준/Gold/11967. 불켜기/불켜기.py
@@ -10,7 +10,7 @@ for i in range(m):
     graph[a1][a2].append([a3, a4])
 
 # 1,1에서 시작 가느아
-visit = [[0] * (m + 3)  for _ in range(n + 1)]
+visit = [[0] * (m + 3) for _ in range(n + 1)]
 light = [[0] * (n + 1) for _ in range(n + 1)]
 
 
@@ -22,22 +22,27 @@ def bfs():
     q.append((1, 1))
     dx = [0, -1, 0, 1]
     dy = [-1, 0, 1, 0]
-    total=[(1,1)]
+
     while q:
         a1, a2 = q.popleft()
+
         for i, j in graph[a1][a2]:
             if light[i][j] == 0:
                 light[i][j] = 1
                 count += 1
-        for z1,z2 in total:
-            for k in range(4):
-                zx = z1 + dx[k]
-                zy = z2 + dy[k]
-                if 0 < zx <= n and 0 < zy <= n:
-                    if light[zx][zy] == 1 and visit[zx][zy] == 0:
-                        visit[zx][zy] = 1
-                        q.append((zx, zy))
-                        total.append((zx,zy))
+                for k in range(4):
+                    zx = i + dx[k]
+                    zy = j + dy[k]
+                    if 0 < zx <= n and 0 < zy <= n:
+                        if visit[zx][zy] == 1:
+                            q.append((zx, zy))
+        for k in range(4):
+            zx = a1 + dx[k]
+            zy = a2 + dy[k]
+            if 0 < zx <= n and 0 < zy <= n:
+                if visit[zx][zy] == 0 and light[zx][zy] == 1:
+                    visit[zx][zy] = 1
+                    q.append((zx, zy))
     print(count)
 
 
