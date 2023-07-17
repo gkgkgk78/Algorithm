d53d9a96728c8929c3c72b0fcf7fcac97dd6ebb2
gkgkgk78 <78834028+gkgkgk78@users.noreply.github.com>
2023-07-17 12:48:28 +0900

    [Gold II] Title: 뉴스 전하기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/1135. 뉴스 전하기/뉴스 전하기.py
+++ 백준/Gold/1135. 뉴스 전하기/뉴스 전하기.py
@@ -10,30 +10,25 @@ graph = [[] for _ in range(n)]
 e = list(map(int, input().split()))
 for i in range(1, len(e)):
     graph[e[i]].append(i)
-count = [0] * (n + 1)
-
 
 def dfs(v):
     visit[v] = 1
     next = []
 
     for a2 in graph[v]:
+        if visit[a2] == 1:
+            continue
         dfs(a2)
         next.append(dp[a2])
     if next:
         next.sort(reverse=True)
-        dp[v] = 0
         t = 0
         last = []
         for i in next:
             t += 1
             last.append(i + t)
         dp[v] = max(last)
-    else:
-        dp[v] = 0
 
 
-visit = [0] * (n)
 dfs(0)
-
 print(max(dp))
