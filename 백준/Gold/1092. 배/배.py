1d548bde5563876d23a4ae594a73291cf251ee9d
gkgkgk78 <78834028+gkgkgk78@users.noreply.github.com>
2023-07-27 22:34:04 +0900

    [Gold V] Title: 배, Time: 3328 ms, Memory: 116504 KB -BaekjoonHub

---
+++ 백준/Gold/1092. 배/배.py
@@ -0,0 +1,34 @@
+import sys
+from collections import deque
+input=sys.stdin.readline
+n=int(input().rstrip())
+crane=list(map(int,input().split()))
+h=int(input().rstrip())
+work=list(map(int,input().split()))
+
+crane.sort(reverse=True)
+work.sort(reverse=True)
+
+now=0
+go=[]
+time=0
+if work[0]>crane[0]:
+    print(-1)
+else:
+    while 1:
+        for i in range(n):
+            n1=crane[i]
+            if len(work)>0:
+                for j in work:
+                    peek=j
+                    if n1>=peek:
+                        work.remove(j)
+                        h-=1
+                        break
+            else:
+                break
+        time+=1
+        go.append(h)
+        if len(work)==0:
+            print(time)
+            break
