3461e5b6149e432e33bdcd2fbe7f2ef719381ca0
gkgkgk78 <78834028+gkgkgk78@users.noreply.github.com>
2023-08-04 12:45:11 +0900

    [Gold II] Title: 모래성, Time: 996 ms, Memory: 162956 KB -BaekjoonHub

---
+++ 백준/Gold/10711. 모래성/모래성.py
@@ -0,0 +1,60 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+time = 0
+
+graph = []
+test = deque()
+for i in range(n):
+    e = list(map(str, input().rstrip()))
+    graph.append(e)
+    for j in range(m):
+        if graph[i][j] == ".":
+            graph[i][j] = 0
+            test.append((i, j))
+        else:
+            graph[i][j] = (int)(graph[i][j])
+
+
+def find(x, y):
+    dx = [-1, -1, -1, 0, 0, 1, 1, 1]
+    dy = [-1, 0, 1, -1, 1, -1, 0, 1]
+    c = 0
+    for i in range(len(dx)):
+        zx = dx[i] + x
+        zy = dy[i] + y
+        if 0 <= zx < n and 0 <= zy < m:
+            if graph[zx][zy] == ".":
+                c += 1
+
+    return c
+
+
+while 1:
+    cnt = 0
+    dx = [-1, -1, -1, 0, 0, 1, 1, 1]
+    dy = [-1, 0, 1, -1, 1, -1, 0, 1]
+    temp = deque()
+    nex = deque()
+    while test:
+        i, j = test.popleft()
+
+        for a in range(len(dx)):
+            zx = dx[a] + i
+            zy = dy[a] + j
+            if 0 <= zx < n and 0 <= zy < m:
+                if graph[zx][zy] != ".":
+                    if 1 <= (int)(graph[zx][zy]) <= 8:
+                        graph[zx][zy] -= 1
+                        if graph[zx][zy] == 0:
+                            nex.append((zx, zy))
+                            cnt += 1
+    if cnt == 0:
+        break
+    time += 1
+    test = nex
+
+print(time)
