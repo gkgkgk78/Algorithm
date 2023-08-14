68885684d616223bd0f8139382ccc6dbfbe4a3d2
gkgkgk78 <78834028+gkgkgk78@users.noreply.github.com>
2023-08-14 11:17:12 +0900

    [Gold I] Title: 로봇 청소기, Time: 1184 ms, Memory: 34752 KB -BaekjoonHub

--- 백준/Gold/4991. 로봇 청소기/로봇 청소기.py
+++ 백준/Gold/4991. 로봇 청소기/로봇 청소기.py
@@ -2,42 +2,34 @@ import sys
 from collections import deque
 
 
-def bfs(x, y, n, m, graph, rx, ry):
-    visit = [[0] * (m) for _ in range(n)]
+def bfs(x, y, graph, dirty, n, m):
+    visit = [[[0] * (1 << 10) for _ in range(m)] for _ in range(n)]
     q = deque()
-    visit[x][y] = 1
     q.append((x, y, 0))
     dx = [-1, 0, 1, 0]
     dy = [0, 1, 0, -1]
 
     while q:
         x1, y1, count = q.popleft()
-        if x1 == rx and y1 == ry:
-            return count
+
+        if count == (1 << len(dirty)) - 1:
+            return visit[x1][y1][count]
         for i in range(4):
             zx = x1 + dx[i]
             zy = y1 + dy[i]
             if 0 <= zx < n and 0 <= zy < m:
-                if visit[zx][zy] == 0 and graph[zx][zy] != "x":
-                    visit[zx][zy] = 1
-                    q.append((zx, zy, count + 1))
+                if graph[zx][zy] != "x":
+                    if graph[zx][zy] == "*" :
+                        nex = count | (1 << dirty.index((zx, zy)))
+                        if visit[zx][zy][nex]==0:
+                            visit[zx][zy][nex] = visit[x1][y1][count] + 1
+                            q.append((zx, zy, nex))
+
+                    else:
+                        if visit[zx][zy][count] == 0:
+                            visit[zx][zy][count] = visit[x1][y1][count] + 1
+                            q.append((zx, zy, count))
     return -1
-
-
-def perm(cnt, visit, check, last, total):
-    if cnt == last:
-        temp = []
-        for i in check:
-            temp.append(i)
-        total.append(temp)
-        return
-    for i in range(last):
-        if visit[i] == 1:
-            continue
-        visit[i] = 1
-        check[cnt] = i
-        perm(cnt + 1, visit, check, last, total)
-        visit[i] = 0
 
 
 while 1:
@@ -46,54 +38,21 @@ while 1:
         break
     graph = []
     dirty = []
-    start = []
+    sx = -1
+    sy = -1
     for i in range(a1):
         e = list(map(str, input().rstrip()))
+        graph.append(e)
         for j in range(a2):
             if e[j] == "*":
-                dirty.append([i, j])
-            if e[j] == "o":
-                start.append((i, j))
-        graph.append(e)
-    total = []
-    visit = [0] * (len(dirty))
-    check = [0] * (len(dirty))
-    perm(0, visit, check, len(dirty), total)
-    # 이제 순서 대로 해서 진행을 하면은 되겠다
-    ans = sys.maxsize
-    total1=[]
-    wall=[[0]*(len(dirty))for _ in range(len(dirty))]
-    for i in range(len(dirty)):
-        nx=dirty[i][0]
-        ny=dirty[i][1]
-        for j in range(len(dirty)):
-            if i==j:
-                continue
-            first = bfs(nx,ny, a1, a2, graph, dirty[j][0], dirty[j][1])
-            wall[i][j]=first
+                dirty.append((i, j))
+            elif e[j] == "o":
+                sx = i
+                sy = j
 
-    total2=[]
-    dirty1=[]
-    for i in dirty:
-        first = bfs(start[0][0], start[0][1], a1, a2, graph, i[0], i[1])
-        total2.append(first)
+    t = bfs(sx, sy, graph, dirty, a1, a2)
 
-    for i1 in range(len(total)):
-        temp = 0
-        tt = 0
-        fin=0
-        if total2[total[i1][0]]==-1:
-            continue
-        for j in range(len(total[i1])-1):
-            if wall[total[i1][j]][total[i1][j + 1]] == -1:
-                fin = 1
-                break
-            temp+=wall[total[i1][j]][total[i1][j+1]]
-
-        temp+=total2[total[i1][0]]
-        if fin==0:
-            ans = min(ans, temp)
-    if ans != sys.maxsize:
-        print(ans)
+    if t != -1:
+        print(t)
     else:
         print(-1)
