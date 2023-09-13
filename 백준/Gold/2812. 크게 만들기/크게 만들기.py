d034a2248cf5e249a088c8bb67227190772cd0ce
gkgkgk78 <78834028+gkgkgk78@users.noreply.github.com>
2023-09-13 11:50:48 +0900

    [Gold III] Title: 크게 만들기, Time: 2724 ms, Memory: 39764 KB -BaekjoonHub

--- 백준/Gold/2812. 크게 만들기/크게 만들기.py
+++ 백준/Gold/2812. 크게 만들기/크게 만들기.py
@@ -5,51 +5,17 @@ from collections import deque
 n,k=map(int,input().split())
 e=list(map(int,input().rstrip()))
 co=0
+stack=[]
 
-ind=1
-ans=deque()
-last=deque()
+
 for i in e:
-    last.append(i)
-ans.append(e[0])
-last.popleft()
-
-while last:
-
-    n1=last.popleft()
-    t=0
-    aa=ans.pop()
-    if aa<n1:
-        ans.append(aa)
-        while 1:
-            if len(ans)>0:
-                ee = ans.pop()
-                if ee<n1:
-                    co+=1
-                    if co==k:
-                        t=1
-                        ans.append(n1)
-                        break
-
-                else:
-                    ans.append(ee)
-                    ans.append(n1)
-                    break
-            else:
-                ans.append(n1)
-                break
-
-    else:
-        ans.append(aa)
-        ans.append(n1)
-
-    if t==1:
-        while last:
-            ans.append(last.popleft())
-        break
-for i in range(k-co):
-    ans.pop()
+    while(co<k and stack and stack[-1]<i):
+        stack.pop()
+        co+=1
+    stack.append(i)
+if k!=co:
+    stack=stack[:-(k-co)]
 an=""
-while ans:
-    an+=(str)(ans.popleft())
+for i in stack:
+    an+=(str)(i)
 print(an)
