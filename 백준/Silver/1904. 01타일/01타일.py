637b968aa9208f989b1086abf9a07fd389a23804
gkgkgk78 <78834028+gkgkgk78@users.noreply.github.com>
2023-09-26 12:21:07 +0900

    [Silver III] Title: 01타일, Time: 156 ms, Memory: 123412 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.py
@@ -0,0 +1,13 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+n=int(input().rstrip())
+dp=[0]*(n+1)
+dp[1]=1
+if n>=2:
+    dp[2]=2
+for i in range(3,n+1):
+    dp[i]=(dp[i-1]+dp[i-2])%15746
+print(dp[-1])
