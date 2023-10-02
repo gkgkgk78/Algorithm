06bd266e3b19af14d0da0efdc3e25030f2faef7c
gkgkgk78 <78834028+gkgkgk78@users.noreply.github.com>
2023-10-02 13:44:19 +0900

    [Gold I] Title: 낚시왕, Time: 1048 ms, Memory: 224252 KB -BaekjoonHub

---
+++ 백준/Gold/17143. 낚시왕/낚시왕.py
@@ -0,0 +1,103 @@
+#45분
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+r, c, m = map(int, input().split())
+graph = [[0] * (c + 1) for _ in range(r + 1)]
+shark = [[[] for _ in range(c + 1)] for _ in range(r + 1)]
+
+for _ in range(m):
+    x, y, speed, dir, size = map(int, input().split())
+    shark[x][y].append([speed, dir, size])
+    # 1위 2아래 3오른 4왼
+ans = 0
+
+
+def death(z):
+    global ans
+    for i in range(1, r + 1):
+        if len(shark[i][z]) == 1:
+            speed, dir, size = shark[i][z][0]
+            shark[i][z] = []
+            ans += size
+            break
+
+
+def go(x, y, speed, dir):
+    # 1위 2아래 3오른 4왼
+    dx = [-1, -1, 1, 0, 0]
+    dy = [-1, 0, 0, 1, -1]
+    if dir == 3 or dir == 4:
+        nex = (speed) % ((c - 1) * 2)
+        # 이제 남은 만큼 이동 해야함
+        zx = x
+        zy = y
+        for i in range(nex):
+
+            zx += dx[dir]
+            zy += dy[dir]
+            if zy == 0 or zy == c + 1:
+                if dir == 3:
+                    dir = 4
+                else:
+                    dir = 3
+                if zy == 0:
+                    zy = 2
+                else:
+                    zy = c - 1
+    else:
+        nex = (speed) % ((r - 1) * 2)
+        zx = x
+        zy = y
+        for i in range(nex):
+
+            zx += dx[dir]
+            zy += dy[dir]
+            if zx == 0 or zx == r + 1:
+                if dir == 1:
+                    dir = 2
+                else:
+                    dir = 1
+                if zx == 0:
+                    zx = 2
+                else:
+                    zx = r - 1
+
+    return [zx, zy, dir]
+
+
+def move():
+    temp = [[[] for _ in range(c + 1)] for _ in range(r + 1)]
+
+    # 이제 하나하나 이동을 하자
+    for i in range(1, r + 1):
+        for j in range(1, c + 1):
+            if len(shark[i][j]) == 1:
+                speed, dir, size = shark[i][j][0]
+                x, y, dd = go(i, j, speed, dir)
+                # 이제 이동 하게 됨
+                temp[x][y].append([speed, dd, size])
+                shark[i][j] = []
+
+    for i in range(1, r + 1):
+        for j in range(1, c + 1):
+            # 상어 잡아 먹기
+            if len(temp[i][j]) > 1:
+                ee = temp[i][j]
+                ee = sorted(ee, key=lambda x: -x[2])
+                shark[i][j] = [ee[0]]
+            elif len(temp[i][j]) == 1:
+                ee = temp[i][j]
+                shark[i][j] = [ee[0]]
+
+
+for z in range(1, c + 1):
+    # 낚시왕 이동
+    # 이제 잡을 거임
+    death(z)
+    # 상어 이동
+    move()
+
+print(ans)
