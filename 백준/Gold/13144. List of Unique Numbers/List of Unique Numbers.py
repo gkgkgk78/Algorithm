a1b60cdfb9c370fc57aa9ee064a0cca67a2e1407
gkgkgk78 <78834028+gkgkgk78@users.noreply.github.com>
2023-10-12 12:51:44 +0900

    [Gold IV] Title: List of Unique Numbers, Time: 160 ms, Memory: 135716 KB -BaekjoonHub

--- 백준/Gold/13144. List of Unique Numbers/List of Unique Numbers.py
+++ 백준/Gold/13144. List of Unique Numbers/List of Unique Numbers.py
@@ -8,14 +8,9 @@ left = 0
 right = 0
 total = dict()
 total[e[right]] = 1
-ans = 1
+ans = 0
 while 1:
-    right += 1
-    if right == n:
-        break
-    if e[right] not in total:
-        total[e[right]] = 0
-    total[e[right]] += 1
+
     if total[e[right]] == 1:
         ans += (right - left + 1)
     else:
@@ -25,6 +20,11 @@ while 1:
             if total[e[left]]==1 and total[e[right]]==1:
                 ans += (right - left + 1)
                 break
-
+    right += 1
+    if right == n:
+        break
+    if e[right] not in total:
+        total[e[right]] = 0
+    total[e[right]] += 1
 
 print(ans)
