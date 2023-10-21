928a7a9cc37e7edfa6df803de1a87586061aeb2a
gkgkgk78 <78834028+gkgkgk78@users.noreply.github.com>
2023-10-21 13:51:45 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 576 ms, Memory: 226812 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.py
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.py
@@ -1,41 +1,38 @@
 import sys
-
+from collections import deque
+import heapq
 
 input = sys.stdin.readline
+
+
 n=int(input().rstrip())
 e=list(map(int,input().split()))
-
-dp=[0]*(n+1)
+dp=[0]*(n)
 dp[0]=e[0]
-index=0
-total=[-1]*(n)
-total[0]=0
-def binary(now):
+
+def bi(val,index):
     left=-1
     right=index+1
+
     while left+1<right:
         mid=(left+right)//2
-        if now<=dp[mid]:
+        if dp[mid]>=val:
             right=mid
+
         else:
             left=mid
+
     return right
 
-for i in range(1,n):
+index=0
+for i in range(1,len(e)):
     now=e[i]
-    if dp[index]<now:
+    if now>dp[index]:
         index+=1
         dp[index]=now
-        total[i]=index
     else:
-        h=binary(now)
-        if h!=-1:
-            total[i]=h
-            dp[h]=now
-last=[-1]*(index+1)
-for i in range(len(total)):
-    t=total[i]
-    if last[t]==-1:
-        last[t]=e[i]
+        a=bi(now,index)
+        if a!=-1:
+            dp[a]=now
 
 print(index+1)
