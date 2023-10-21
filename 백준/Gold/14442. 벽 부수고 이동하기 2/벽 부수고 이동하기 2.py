108b9e533dd657ec04b5bc3456c5c4c91f296313
gkgkgk78 <78834028+gkgkgk78@users.noreply.github.com>
2023-10-21 12:22:21 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 5460 ms, Memory: 471696 KB -BaekjoonHub

--- 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.py
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.py
@@ -1,56 +1,53 @@
-import sys, copy, heapq
-import heapq, math
-from itertools import permutations, combinations, product
+import sys
 from collections import deque
-from itertools import product
-#sys.setrecursionlimit(10 ** 5)
-#a=list(product(i,repeat=len(powers)))
-#b=list(product(*a)) #리스트 안에 있는 원소들끼리 조합
+import heapq
 
+input = sys.stdin.readline
 
-#input = sys.stdin.readline
+n, m,k = map(int, input().split())
+graph = []
+for _ in range(n):
+    a1 = list(map(int, input().rstrip()))
+    graph.append(a1)
+visit = [ [ [sys.maxsize] * (k+1) for _ in range(m)] for i in range(n)]
 
-from itertools import combinations_with_replacement as cwr
-from collections import Counter
-
-
-
-def bfs(a,b):
+def bfs():
     q=deque()
-    q.append((a,b,0))
-    dx = [0, 0, 1, -1]
-    dy = [1, -1, 0, 0]
-    visit[a][b][0]=1
+    q.append((0,0,0))
+
+    dx=[-1,0,1,0]
+    dy=[0,1,0,-1]
+    visit[0][0][0]=1
+
     while q:
-        a1,a2,a3=q.popleft()
-        if a1==n-1 and a2==m-1:
-            return visit[a1][a2][a3]
+        x,y,count=q.popleft()
+        now=visit[x][y][count]
         for i in range(4):
-            zx,zy=a1+dx[i],a2+dy[i]
-            if 0<=zx<n and 0<=zy<m :
-                if graph[zx][zy]==1 and a3<k:
-                    if visit[zx][zy][a3+1] ==0:
-                        visit[zx][zy][a3+1]=visit[a1][a2][a3]+1
-                        q.append((zx,zy,a3+1))
-                elif graph[zx][zy]==0 and visit[zx][zy][a3]==0:
-                    visit[zx][zy][a3]=visit[a1][a2][a3]+1
-                    q.append((zx,zy,a3))
-    return -1
-n,m,k=map(int,input().split())
-visit = [[[0] * (k+1) for _ in range(m)] for _ in range(n)]
-graph=[]
-wall=[]
-for j in range(n):
-    a=list(map(int,input().rstrip()))
-    graph.append(a)
-aw=99999
+            zx=x+dx[i]
+            zy=y+dy[i]
+            if 0<=zx<n and 0<=zy<m:
+                if zx==n-1 and zy==m-1 and visit[zx][zy][count]==sys.maxsize:
+                    visit[zx][zy][count]=now+1
+                    continue
+                if graph[zx][zy]==0:
+                    if visit[zx][zy][count]==sys.maxsize:
+                        visit[zx][zy][count]=now+1
+                        q.append((zx,zy,count))
 
-t=bfs(0,0)
+                else:
+                    if count <k:
+                        if visit[zx][zy][count+1] == sys.maxsize:
+                            visit[zx][zy][count+1]=now+1
+                            q.append((zx, zy, count+1))
 
-if t==-1:
+
+bfs()
+ans=sys.maxsize
+for i in visit[n-1][m-1]:
+    if i==sys.maxsize:
+        continue
+    ans=min(ans,i)
+if ans==sys.maxsize:
     print(-1)
 else:
-    print(t)
-
-
-
+    print(ans)
