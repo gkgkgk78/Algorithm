b1b3731cd2bc0f6aeede851e4a5fd577d15b115c
gkgkgk78 <78834028+gkgkgk78@users.noreply.github.com>
2023-10-21 17:35:01 +0900

    [Gold V] Title: 회문, Time: 208 ms, Memory: 116252 KB -BaekjoonHub

--- 백준/Gold/17609. 회문/회문.py
+++ 백준/Gold/17609. 회문/회문.py
@@ -1,38 +1,52 @@
 import sys
+from collections import deque
+import heapq
 
 input = sys.stdin.readline
 
 n=int(input().rstrip())
 
-def check(e,left,right):
+def check(e):
 
+    if len(e)%2==0:
+        left=e[:len(e)//2]
+        right=e[len(e)//2:]
+        right=right[::-1]
+        if left==right:
+            return 1
+    else:
+        left=e[:len(e)//2]
+        right=e[len(e)//2+1:]
+        right=right[::-1]
+        if left==right:
+            return 1
+    return 0
+
+for _ in range(n):
+    e=str(input().rstrip())
+    a1=check(e)
+    if a1==1:
+        print(0)
+        continue
+
+    left=0
+    right=len(e)-1
+    tt=0
     while left<right:
+
         if e[left]==e[right]:
             left+=1
             right-=1
         else:
-            return 0
-    return 1
-
-
-for _ in range(n):
-    e=list(map(str,input().rstrip()))
-    left=0
-    right=len(e)-1
-
-    count=0
-    while left<right:
-        if e[left]==e[right]:
-            left += 1
-            right -= 1
-        else:
-            a1=check(e,left+1,right)
-            a2=check(e,left,right-1)
-            if a1 or a2:
-                count=1
-                break
-            else:
-                count=2
-                break
-
-    print(count)
+            #왼쪽 제거
+            now=e[:left]+e[left+1:]
+            #오른쪽 제거
+            now1 = e[:right] + e[right + 1:]
+            a1=check(now)
+            a2=check(now1)
+            if a1==1 or a2==1:
+                print(1)
+                tt=1
+            break
+    if tt==0:
+        print(2)
