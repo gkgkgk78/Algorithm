e92bd90b63b58ff38a21449bb9b98c5fe1960613
gkgkgk78 <78834028+gkgkgk78@users.noreply.github.com>
2023-10-30 10:28:08 +0900

    [Gold III] Title: Dance Dance Revolution, Time: 2524 ms, Memory: 147112 KB -BaekjoonHub

--- 백준/Gold/2342. Dance Dance Revolution/Dance Dance Revolution.py
+++ 백준/Gold/2342. Dance Dance Revolution/Dance Dance Revolution.py
@@ -1,44 +1,36 @@
 import sys
 sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
-INF = float("inf")
 
-N = 1
-Arr = []
-DP = [[[INF] * 5 for _ in range(5)] for _ in range(100002)]
-Cost = [
+dp = [[[sys.maxsize] * 5 for _ in range(5)] for _ in range(100001)]
+cost = [
     [1, 2, 2, 2, 2],
     [2, 1, 3, 4, 3],
     [2, 3, 1, 3, 4],
     [2, 4, 3, 1, 3],
     [2, 3, 4, 3, 1]
 ]
+e = list(map(int, input().split()))
+e = e[:-1]
 
 
-def D(i, l, r):
-    if i >= N:
+def dfs(cnt, left, right):
+
+    if cnt == len(e):
+        # 이때는 무언갈 하겠지
         return 0
-    if DP[i][l][r] != INF:
-        return DP[i][l][r]
-    rc = 0
-    lc = 0
-    if i + 1 < N:
-        rc = Cost[r][Arr[i + 1]]
-        lc = Cost[l][Arr[i + 1]]
 
-    if Arr[i + 1] != r:
-        DP[i][l][r] = min(DP[i][l][r], D(i + 1, Arr[i + 1], r) + lc)
-    if Arr[i + 1] != l:
-        DP[i][l][r] = min(DP[i][l][r], D(i + 1, l, Arr[i + 1]) + rc)
-    return DP[i][l][r]
+    if dp[cnt][left][right] != sys.maxsize:
+        return dp[cnt][left][right]
 
+    l=cost[left][e[cnt]]
+    r=cost[right][e[cnt]]
 
-ee = list(map(int, input().split()))
-Arr=[0]*(len(ee)+2)
-for i in ee:
-    if i == 0:
-        break
-    Arr[N] = i
-    N += 1
+    if e[cnt]!=right:
+        dp[cnt][left][right]=min(dp[cnt][left][right],dfs(cnt+1,e[cnt],right)+l)
+    if e[cnt]!=left:
+        dp[cnt][left][right]=min(dp[cnt][left][right],dfs(cnt+1,left,e[cnt])+r)
 
-print(D(0, 0, 0))
+    return dp[cnt][left][right]
+
+print(dfs(0,0,0))
