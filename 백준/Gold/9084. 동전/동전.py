73419b4beaae952a735a5183b0113bd71b1d6234
gkgkgk78 <78834028+gkgkgk78@users.noreply.github.com>
2023-10-31 11:47:47 +0900

    [Gold V] Title: 동전, Time: 140 ms, Memory: 113164 KB -BaekjoonHub

--- 백준/Gold/9084. 동전/동전.py
+++ 백준/Gold/9084. 동전/동전.py
@@ -1,17 +1,17 @@
 import sys
 from collections import deque
+from itertools import combinations
 
 input = sys.stdin.readline
 
-t = int(input().rstrip())
+t=int(input().rstrip())
 for _ in range(t):
-    n = int(input().rstrip())
-    e = list(map(int, input().split()))
-    k = int(input().rstrip())
-    dp = [0] * (k + 1)
+    _=int(input().rstrip())
+    e=list(map(int,input().split()))
+    l=int(input().rstrip())
+    dp=[0]*(l+1)
     dp[0]=1
-    for i in range(n):
-        now=e[i]
-        for j in range(now,k+1):
-            dp[j]+=dp[j-now]
-    print(dp[k])
+    for i in e:
+        for j in range(i,l+1):
+            dp[j]+=dp[j-i]
+    print(dp[l])
