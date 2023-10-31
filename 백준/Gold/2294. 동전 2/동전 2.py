e6e679a06a4ff1cdd7d6b3e7884cba7945530b3a
gkgkgk78 <78834028+gkgkgk78@users.noreply.github.com>
2023-10-31 12:05:42 +0900

    [Gold V] Title: 동전 2, Time: 144 ms, Memory: 113884 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.py
+++ 백준/Gold/2294. 동전 2/동전 2.py
@@ -1,19 +1,22 @@
 import sys
 from collections import deque
+from itertools import combinations
 
 input = sys.stdin.readline
-n,m=map(int,input().split())
+
+n,k=map(int,input().split())
+
+
 e=[]
 for _ in range(n):
     e.append(int(input().rstrip()))
 e.sort()
-dp=[sys.maxsize]*(m+1)
+dp=[sys.maxsize]*(k+1)
 dp[0]=0
-for i in range(n):
-    now=e[i]
-    for j in range(now,m+1):
-        dp[j]=min(dp[j],dp[j-now]+1)
-if dp[m]==sys.maxsize:
+for i in e:
+    for j in range(i,k+1):
+        dp[j]=min(dp[j-i]+1,dp[j])
+if dp[k]==sys.maxsize:
     print(-1)
 else:
-    print(dp[m])
+    print(dp[k])
