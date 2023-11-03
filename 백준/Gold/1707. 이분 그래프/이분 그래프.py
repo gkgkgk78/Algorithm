ce1960a8c206325546c9b7db08fb5e2c2a50a853
gkgkgk78 <78834028+gkgkgk78@users.noreply.github.com>
2023-11-03 12:13:26 +0900

    [Gold IV] Title: 이분 그래프, Time: 788 ms, Memory: 218236 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.py
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.py
@@ -1,50 +1,54 @@
 import sys
 from collections import deque
+from itertools import combinations
 
 input = sys.stdin.readline
-t=int(input().rstrip())
 
-def bfs(vertex,what,color,visit,graph):
 
-    visit[vertex]=1
+#그래프를 둘로 분할하여서 각 집합에 속한 정점 끼리는 인접 하지 않도록 분할 하자
+#이분 그래프 이다.
+
+#이분 그래프 인지 아닌지 판별 하도록 하라
+
+
+def bfs(ver,visit):
     q=deque()
-    color[vertex]=0
-    q.append(vertex)
+    visit[ver]=1
+    q.append((ver,1))
     while q:
-        a1=q.popleft()
-        ne=-1
-        if color[a1]==0:
-            ne=1
-        else:
-            ne=0
-        for i in graph[a1]:
+        vertex,color=q.popleft()
+        for i in graph[vertex]:
             if visit[i]==0:
-                visit[i]=1
-                color[i]=ne
-                q.append(i)
+                if color==1:
+                    visit[i]=2
+                    q.append((i, 2))
+                else:
+                    visit[i]=1
+                    q.append((i, 1))
 
 
-
-for _ in range(t):
+k=int(input().rstrip())
+for _ in range(k):
     v,e=map(int,input().split())
-    graph=[[]for _ in range(v+1)]
+    #정점의 개수와 간선의 개수가 주어 졌다
     visit=[0]*(v+1)
-    total=[]
-    for _ in range(e):
+    edge=[]
+    graph=[[]for _ in range(v+1)]
+    for i in range(e):
         a1,a2=map(int,input().split())
+        edge.append((a1,a2))
         graph[a1].append(a2)
         graph[a2].append(a1)
-        total.append((a1,a2))
-    color=[-1]*(v+1)
+
     for i in range(1,v+1):
         if visit[i]==0:
-            bfs(i,0,color,visit,graph)
-    tt=0
-    for a1,a2 in total:
-        if color[a1]==color[a2]:
-            tt=1
+            bfs(i,visit)
+    check=0
+    for a1,a2 in edge:
+        if visit[a1]==visit[a2]:
+            check=1
             break
-    if tt==0:
+    if check==0:
         print("YES")
     else:
         print("NO")
