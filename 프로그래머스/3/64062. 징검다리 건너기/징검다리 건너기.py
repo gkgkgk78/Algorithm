c4f43c86ce32b2fb7c8ee7dfb39506e1928c825c
gkgkgk78 <78834028+gkgkgk78@users.noreply.github.com>
2023-11-21 15:09:32 +0900

    [level 3] Title: 징검다리 건너기, Time: 188.72 ms, Memory: 18.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.py
@@ -0,0 +1,43 @@
+import sys
+def game(stones,k,mid):
+    
+
+    temp=0
+    for i in range(len(stones)):
+        if stones[i]<mid:
+            temp+=1
+            if temp>=k:
+                return -1
+        else:
+            temp=0
+    
+    return 1
+
+
+def finds(stones,k):
+
+    
+    left=0
+    right=max(stones)+1
+    mama=-1
+    while left+1<right:
+        mid=(left+right)//2
+        aa=game(stones,k,mid)
+        #print(left,right)
+        if aa==1:
+            left=mid
+            mama=max(mama,left)
+        else:
+            right=mid
+    return mama
+            
+    
+    
+
+def solution(stones, k):
+    answer = 0
+    
+    answer=finds(stones,k)
+    
+    
+    return answer
