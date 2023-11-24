5bb5b89e72de6ff69f98834bb2a6e10517fc6f69
gkgkgk78 <78834028+gkgkgk78@users.noreply.github.com>
2023-11-24 15:07:03 +0900

    [Gold IV] Title: 부분합, Time: 140 ms, Memory: 120932 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.py
+++ 백준/Gold/1806. 부분합/부분합.py
@@ -1,34 +1,34 @@
 import sys
-from collections import deque
 
 input = sys.stdin.readline
 
-ans = sys.maxsize
 n, s = map(int, input().split())
 e = list(map(int, input().split()))
 
 left = 0
-right = 1
-sumz = e[0] + e[1]
+right = 0
+now = e[0]
+
+ans = sys.maxsize
+if now >= s:
+    ans = min(ans, right - left + 1)
 
 while right < n:
-    if sumz >= s:
-        while sumz >= s:
-            ans = min(ans, right - left + 1)
-            sumz -= e[left]
-            left += 1
+    right += 1
+    if right == n:
+        break
+    now+=e[right]
+    if now>=s:
+        while 1:
+            if now >= s:
+                ans = min(ans, right - left + 1)
             if left == right:
                 break
-    else:
-        right += 1
-        if right == n:
-            break
-        sumz += e[right]
-for i in e:
-    if i >= s:
-        ans = 1
-        break
-if ans == sys.maxsize:
+            now-=e[left]
+            left+=1
+            if now<s:
+                break
+if ans==sys.maxsize:
     print(0)
 else:
     print(ans)
