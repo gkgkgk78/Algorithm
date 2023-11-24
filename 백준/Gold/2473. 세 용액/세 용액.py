ae35dd2ad70320f1b0fa7b9bc505e5ee9bb88dad
gkgkgk78 <78834028+gkgkgk78@users.noreply.github.com>
2023-11-24 14:47:05 +0900

    [Gold III] Title: 세 용액, Time: 160 ms, Memory: 110576 KB -BaekjoonHub

--- 백준/Gold/2473. 세 용액/세 용액.py
+++ 백준/Gold/2473. 세 용액/세 용액.py
@@ -1,27 +1,39 @@
 import sys
 
 input = sys.stdin.readline
-
-
-n=int(input().rstrip())
-e=list(map(int,input().split()))
+n = int(input().rstrip())
+e = list(map(int, input().split()))
+answer=sys.maxsize
 e.sort()
-ans=[]
-an=sys.maxsize
-for i in range(n-2):
-    now=e[i]
-    left=i+1
-    right=n-1
-    while left<right:
-        sumz=e[i]+e[left]+e[right]
-        if abs(sumz) < an:
-            ans=[e[i],e[left],e[right]]
-            an=abs(sumz)
-        if sumz==0:
-          print(e[i],e[left],e[right])
-          sys.exit(0)
-        elif sumz<0:
-          left+=1
+
+ans=[-1,-1,-1]
+for i in range(1, n - 1):
+
+    left = 0
+    right = n - 1
+    now = e[i]+e[left]+e[right]
+
+    while 1:
+        if now == 0:
+            print(e[left], e[i], e[right])
+            sys.exit(0)
         else:
-          right-=1
+            if abs(now) < answer:
+                answer = abs(now)
+                ans[0] = e[left]
+                ans[1] = e[i]
+                ans[2] = e[right]
+        if now<0:
+            now-=e[left]
+            left+=1
+            if left==i:
+                break
+            now+=e[left]
+        elif now>0:
+            now -= e[right]
+            right -= 1
+            if right == i:
+                break
+            now += e[right]
+
 print(*ans)
