5f058e3d01deeaf7833714f444f90cf6113dbcb4
gkgkgk78 <78834028+gkgkgk78@users.noreply.github.com>
2023-11-26 12:37:47 +0900

    [level 3] Title: 등산코스 정하기, Time: 573.56 ms, Memory: 88.1 MB -BaekjoonHub

--- 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.py
+++ 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.py
@@ -5,7 +5,7 @@ import heapq
 #dijk(gates,graph,fin,n)
 def dijk(gates,graph,fin,n):
     visit=[sys.maxsize]*(n+1)
-
+    #다익스트라 풀이
     q=[]
     for i in gates:
         visit[i]=0
@@ -31,14 +31,10 @@ def dijk(gates,graph,fin,n):
 def solution(n, paths, gates, summits):
     answer = []
     #산봉 우리중 한곳만 방문한뒤 원래의 출입구로 돌아오는 등산코스를 정하려고 한다
-    graph=dict()
+    graph=[[]for _ in range(n+1)]
     la=[]
     for i in paths:
         a1,a2,a3=i
-        if a1 not in graph:
-            graph[a1]=[]
-        if a2 not in graph:
-            graph[a2]=[]
         graph[a1].append((a2,a3))
         graph[a2].append((a1,a3))
     #gates는 출입구임
