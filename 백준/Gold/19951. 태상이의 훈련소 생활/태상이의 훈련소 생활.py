c917450e0e9a8cf3dd298f56f2d17f35696b3ace
gkgkgk78 <78834028+gkgkgk78@users.noreply.github.com>
2024-06-30 12:49:52 +0900

    [Gold V] Title: 태상이의 훈련소 생활, Time: 248 ms, Memory: 41932 KB -BaekjoonHub

--- 백준/Gold/19951. 태상이의 훈련소 생활/태상이의 훈련소 생활.py
+++ 백준/Gold/19951. 태상이의 훈련소 생활/태상이의 훈련소 생활.py
@@ -1,19 +1,17 @@
 import sys
-
 input = sys.stdin.readline
-n,m = map(int,input().split())
+
+n,m=map(int,input().split())
 e=list(map(int,input().split()))
-
-sumz=[0]*(n+1)
-
+data=[0]*(n+1)
 for _ in range(m):
-    a,b,k=map(int,input().split())
-    sumz[a-1]+=k
-    sumz[b]-=k
-
-for i in range(1,n):
-    sumz[i]+=sumz[i-1]
-
+    a1,a2,a3=map(int,input().split())
+    a1-=1
+    a2-=1
+    data[a1]+=a3
+    data[a2+1]-=a3
+for i in range(1,n+1):
+    data[i]+=data[i-1]
 for i in range(n):
-    e[i]+=sumz[i]
+    e[i]+=data[i]
 print(*e)
