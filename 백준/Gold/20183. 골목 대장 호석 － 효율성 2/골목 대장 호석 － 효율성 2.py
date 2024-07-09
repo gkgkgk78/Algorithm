57723baca744639479a0645b22ab523647a940e1
gkgkgk78 <78834028+gkgkgk78@users.noreply.github.com>
2024-07-09 21:53:07 +0900

    [Gold II] Title: 골목 대장 호석 - 효율성 2, Time: 4412 ms, Memory: 228556 KB, Score: 43 point -BaekjoonHub

--- 백준/Gold/20183. 골목 대장 호석 － 효율성 2/골목 대장 호석 － 효율성 2.py
+++ 백준/Gold/20183. 골목 대장 호석 － 효율성 2/골목 대장 호석 － 효율성 2.py
@@ -1,59 +1,54 @@
 import sys
+from collections import deque
 import heapq
-
 input = sys.stdin.readline
 
-n, m, start, last, value = map(int, input().split())
-graph = [[] for _ in range(n + 1)]
-cc=[]
+n,m,a,b,c=map(int,input().split())
+
+graph=[[]for _ in range(n+1)]
+left=0
 right=-1
 for _ in range(m):
-    a1, a2, a3 = map(int, input().split())
-    heapq.heappush(cc,-a3)
-    graph[a1].append((a2, a3))
-    graph[a2].append((a1, a3))
+    a1,a2,a3=map(int,input().split())
+    graph[a1].append((a2,a3))
+    graph[a2].append((a1,a3))
+    right=max(right,a3)
+right+=1
 
-
-
-
-def dijk(start, last, tt):
-
-    q = []
-    distance = [sys.maxsize] * (n + 1)
-    heapq.heappush(q, (0, start))
-    distance[start] = 0
-
+def bfs(mid):
+    distance=[sys.maxsize]*(n+1)
+    q=[]
+    heapq.heappush(q,(0,a))
+    distance[a]=0
     while q:
-        val, ver = heapq.heappop(q)
-        if distance[ver] < val:
+        value,vertex=heapq.heappop(q)
+        if distance[vertex]<value:
             continue
-        for go, va in graph[ver]:
-            if va > tt:
+        for a1,a2 in graph[vertex]:
+            if a2>mid:
                 continue
-            if va + val <= value:
-                if distance[go] > va + val:
-                    distance[go] = va + val
-                    if go != last:
-                        heapq.heappush(q, (va + val, go))
-    if distance[last]<=value and distance[last]!=-1:
+            nex=a2+value
+            if nex<=c:
+                if distance[a1]>nex:
+                    distance[a1]=nex
+                    heapq.heappush(q,(nex,a1))
+
+    if distance[b]<=c:
         return 1
     else:
         return 0
 
-left = 0
-right=(-heapq.heappop(cc))+1
-ans = sys.maxsize
-while left + 1 < right:
 
-    mid = (left + right) // 2
-    aa = dijk(start, last, mid)
-    if aa ==1:
-        right = mid
-        ans = min(ans, mid)
+ans=sys.maxsize
+while left+1<right:
+    mid=(left+right)//2
+    a1=bfs(mid)
+    if a1==1:
+        right=mid
+        ans=min(ans,right)
     else:
-        left = mid
-
-if ans == sys.maxsize:
+        left=mid
+if ans==sys.maxsize:
     print(-1)
 else:
     print(ans)
