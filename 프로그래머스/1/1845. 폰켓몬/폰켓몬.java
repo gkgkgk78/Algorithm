03fc2d6a04685b661043b1b972dd374591f4b314
gkgkgk78 <78834028+gkgkgk78@users.noreply.github.com>
2024-08-10 18:43:23 +0900

    [level 1] Title: 폰켓몬, Time: 2.90 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -3,15 +3,24 @@ import java.util.*;
 class Solution {
     public int solution(int[] nums) {
         int answer = 0;
-        //n/2 마리 가져 가도 좋다
-        HashSet<Integer>set=new HashSet<>();
-        for(Integer a:nums)
-            set.add(a);
-        if(set.size()>=nums.length/2)
-            answer= nums.length/2;
+        //같은 종류의 폰켓몬은 같은 번호를 가지고 있다
+        HashMap<Integer,Integer>map=new HashMap<>();
+        int count=0;
+        for (int i=0;i<nums.length;i++){
+            int now=nums[i];
+            if(!map.containsKey(now)){
+                count+=1;
+                map.put(now,1);
+            }   
+        }
+        int test=nums.length/2;
+        if(count>=test)
+        {
+            return test;
+        }
         else
-            answer=set.size();
+            return count;
+
         
-        return answer;
     }
 }
