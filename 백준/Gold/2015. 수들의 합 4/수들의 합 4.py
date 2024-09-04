dd3c12ea75521c00b4c6b2170cfa72e924220b94
gkgkgk78 <78834028+gkgkgk78@users.noreply.github.com>
2024-09-04 22:35:52 +0900

    [Gold IV] Title: 수들의 합 4, Time: 172 ms, Memory: 46352 KB -BaekjoonHub

--- 백준/Gold/2015. 수들의 합 4/수들의 합 4.py
+++ 백준/Gold/2015. 수들의 합 4/수들의 합 4.py
@@ -1,25 +1,24 @@
 import sys
-
 input = sys.stdin.readline
-n, k = map(int, input().split())
-e = list(map(int, input().split()))
 
-find = dict()
-find[e[0]] = 1
-ans = 0
-if e[0] == k:
-    ans += 1
-
-for l in range(1, n):
-    e[l] += e[l - 1]
-    next = e[l] - k
-    if next in find:
-        ans += find[next]
-    if e[l] not in find:
-        find[e[l]] = 1
+n,k=map(int,input().split())
+e=list(map(int,input().split()))
+data=[0]*(n)
+data[0]=e[0]
+test=dict()
+test[e[0]]=1
+answer=0
+if e[0]==k:
+    answer+=1
+for i in range(1,n):
+    data[i]=data[i-1]+e[i]
+    if data[i]==k:
+        answer+=1
+    now=data[i]-k
+    if now in test:
+        answer+=test[now]
+    if data[i] in test:
+        test[data[i]]+=1
     else:
-        find[e[l]] += 1
-    if e[l] == k:
-        ans += 1
-
-print(ans)
+        test[data[i]]=1
+print(answer)
