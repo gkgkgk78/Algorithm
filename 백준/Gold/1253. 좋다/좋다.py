bf007c0b4a6461349ec440283d71d277a04c4c08
gkgkgk78 <78834028+gkgkgk78@users.noreply.github.com>
2024-09-04 22:12:54 +0900

    [Gold IV] Title: 좋다, Time: 856 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.py
+++ 백준/Gold/1253. 좋다/좋다.py
@@ -1,32 +1,28 @@
 import sys
-from copy import deepcopy
 input = sys.stdin.readline
 
 n=int(input().rstrip())
 e=list(map(int,input().split()))
-e.sort()
-
-
 answer=0
 
-def check(left,right,temp,now):
-
+e.sort()
+for i in range(n):
+    left=0
+    right=n-1
+    now=e[i]
     while left<right:
-        mid=(temp[left]+temp[right])
-        if mid>now:
+        temp=e[left]+e[right]
+        if temp==now:
+            if left==i:
+                left+=1
+            elif right==i:
+                right-=1
+            else:
+                answer+=1
+                break
+        elif temp>now:
             right-=1
         else:
-            if mid==now:
-                return 1
             left+=1
-    return 0
 
-answer=0
-for l in range(n):
-    temp=deepcopy(e)
-    temp.pop(l)
-
-    now=check(0,n-2,temp,e[l])
-    if now==1:
-        answer+=1
 print(answer)
