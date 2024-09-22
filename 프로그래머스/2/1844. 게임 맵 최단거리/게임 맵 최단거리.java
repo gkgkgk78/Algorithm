1da5bb447a9a2139604940310fbea0ea0e28be71
gkgkgk78 <78834028+gkgkgk78@users.noreply.github.com>
2024-09-22 21:28:10 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 13.00 ms, Memory: 52.9 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,73 +1,63 @@
 import java.util.*;
-
-
-
 class Solution {
     
     public class go{
-        int x,y,count;
         
-        public go(int x,int y, int count)
-        {
+        int x,y,size;
+        public go(int x, int y, int size){
             this.x=x;
             this.y=y;
-            this.count=count;
+            this.size=size;
         }
+        
         
     }
     
     
-    
-    
-    public int bfs(int [][]graph){
-        Queue<go>q=new LinkedList<>();
-        int [][]visit=new int[graph.length][graph[0].length];
-        int answer=-1;
-        visit[0][0]=1;
+    public int bfs(int [][]maps){
+     
+        PriorityQueue<go>q=new PriorityQueue<>((x,y)->{
+            return x.size-y.size;
+        });
+        int n=maps.length;
+        int m=maps[0].length;
+        int visit[][]=new int[maps.length][maps[0].length];
         q.add(new go(0,0,1));
-        int n=graph.length;
-        int m=graph[0].length;
+        visit[0][0]=1;
         int dx[]={-1,0,1,0};
         int dy[]={0,1,0,-1};
-        while(q.size()>0)
-        {
-            go a=q.poll();
-            if (a.x==n-1 &&a.y==m-1)
-            {
-                return a.count;
-            }
-            for(int i=0;i<4;i++)
-            {
-                int zx,zy;
-                zx=dx[i]+a.x;
-                zy=dy[i]+a.y;
-                if(0<=zx && zx<n && 0<=zy && zy<m)
-                {
-                    if(graph[zx][zy]==1 &&visit[zx][zy]==0)
-                    {
-                        visit[zx][zy]=1;
-                        q.add(new go(zx,zy,a.count+1));
+        while(!q.isEmpty()){
+            go now=q.poll();
+            if(now.x==n-1 && now.y==m-1)
+                continue;
+            for(int i=0;i<4;i++){
+                int zx=dx[i]+now.x;
+                int zy=dy[i]+now.y;
+                if (0<=zx&&zx<n &&0<=zy&&zy<m &&maps[zx][zy]==1){
+                    if(visit[zx][zy]==0){
+                        visit[zx][zy]=now.size+1;
+                        q.add(new go(zx,zy,now.size+1));
+                    }
+                    else{
+                        if(now.size+1<visit[zx][zy]){
+                            visit[zx][zy]=now.size+1;
+                            q.add(new go(zx,zy,now.size+1));
+                        }
                     }
                 }
             }
-        
+            
         }
-        
-        
-        
-        
-        
-        
-        return answer;
+        if(visit[n-1][m-1]==0)
+            return -1;
+        return visit[n-1][m-1];
     }
     
     
     
     public int solution(int[][] maps) {
         int answer = 0;
-        answer=bfs(maps);
-        
-        
-        return answer;
+        return bfs(maps);
     }
+    
 }
