5b82c3955ffa5ba8d72e244a21d311fdeda66d7e
gkgkgk78 <78834028+gkgkgk78@users.noreply.github.com>
2024-10-04 20:48:59 +0900

    [Gold III] Title: 골목 대장 호석 - 효율성 1, Time: 7484 ms, Memory: 258952 KB, Score: 16 point -BaekjoonHub

--- 백준/Gold/20182. 골목 대장 호석 － 효율성 1/골목 대장 호석 － 효율성 1.py
+++ 백준/Gold/20182. 골목 대장 호석 － 효율성 1/골목 대장 호석 － 효율성 1.py
@@ -5,7 +5,7 @@ input = sys.stdin.readline
 
 n, m, start, last, value = map(int, input().split())
 graph = [[] for _ in range(n + 1)]
-
+before = [[] for _ in range(n + 1)]
 
 for _ in range(m):
     a1, a2, a3 = map(int, input().split())
@@ -13,18 +13,18 @@ for _ in range(m):
     graph[a1].append((a2, a3))
     graph[a2].append((a1, a3))
 
-
+ans = sys.maxsize
 
 
 def dijk(start, last, tt):
-
+    global ans
     q = []
     distance = [sys.maxsize] * (n + 1)
-    heapq.heappush(q, (0, start))
+    heapq.heappush(q, (0, start, 0))
     distance[start] = 0
 
     while q:
-        val, ver = heapq.heappop(q)
+        val, ver, now = heapq.heappop(q)
         if distance[ver] < val:
             continue
         for go, va in graph[ver]:
@@ -33,14 +33,17 @@ def dijk(start, last, tt):
             if va + val <= value:
                 if distance[go] > va + val:
                     distance[go] = va + val
+                    before[go].append(ver)
                     if go != last:
-                        heapq.heappush(q, (va + val, go))
-
+                        heapq.heappush(q, (va + val, go, max(now, va)))
+                    else:
+                        a2 = max(now, va)
+                        ans = min(ans, a2)
     return distance[last]
 
 
 left = 0
-right = 21
+right = 1001
 ans = sys.maxsize
 while left + 1 < right:
 
