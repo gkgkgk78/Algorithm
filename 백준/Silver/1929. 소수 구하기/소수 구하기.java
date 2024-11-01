54dd1d0d50764fad6ec6aaaecf331d615834c32d
gkgkgk78 <78834028+gkgkgk78@users.noreply.github.com>
2024-11-01 14:38:57 +0900

    [Silver III] Title: 소수 구하기, Time: 252 ms, Memory: 23300 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,29 +1,38 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static StringBuffer buffer;
-	public static void main(String[] args) {
 
-		Scanner sc = new Scanner(System.in);
 
-		int n = sc.nextInt();
-		int m = sc.nextInt();
-		buffer = new StringBuffer();
-		int arr[] = new int[m + 1];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer s = new StringTokenizer(br.readLine(), " ");
+        int n,m;
+        n=Integer.parseInt(s.nextToken());
+        m=Integer.parseInt(s.nextToken());
+        boolean digit[]=new boolean[m+1];
+        digit[0]=true;//false: 소수 아님
+        digit[1]=true;
+        for (int i=2;i<=Math.sqrt(m);i++){
+            if (digit[i])
+                continue;
+            int count=2;
+            while (true){
+                int now=i*count;
+                if (now>m)
+                    break;
+                digit[now]=true;
+                count+=1;
+            }
+        }
+        StringBuilder sb=new StringBuilder();
+        for(int i=n;i<=m;i++){
+            if (!digit[i])
+                sb.append(i+"\n");
+        }
+        System.out.println(sb);
 
-		for (int i = 2; i <= m; i++) {
-			int j = i;
-			while (j <= m) {
-				arr[j] += 1;
-				j = j + i;
-			}
 
-		}
-		for (int i = n; i <= m; i++) {
-			if (arr[i] == 1)
-				buffer.append(i+"\n");
-		}
-		System.out.println(buffer);
-	}
+    }
 
 }
