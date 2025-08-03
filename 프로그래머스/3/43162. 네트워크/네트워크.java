19fdda2b121c330b591871cdd2d612394efdab03
gkgkgk78 <78834028+gkgkgk78@users.noreply.github.com>
2025-08-03 23:01:52 +0900

    [level 3] Title: 네트워크, Time: 0.57 ms, Memory: 81.7 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,37 +1,38 @@
 import java.util.*;
+
 class Solution {
     
-    public void bfs(int []visit,int[][]computers,int vertex,int n){
-        Deque<Integer>q=new LinkedList<>();
-        visit[vertex]=1;
-        q.add(vertex);
-        while(!q.isEmpty()){
-            Integer now=q.poll();
-            for (int i=0;i<n;i++){
-                if(computers[now][i]==1 && visit[i]==0){
+    static int visit[];
+    
+    public void bfs(int start,int n, int[][]computers){
+        
+        Deque<Integer>queue=new LinkedList<>();
+        visit[start]=1;
+        queue.add(start);
+        while(!queue.isEmpty()){
+            int now=queue.poll();
+            for(int i=0;i<computers[now].length;i++){
+                int temp=computers[now][i];
+                if(temp==1&&visit[i]==0){
                     visit[i]=1;
-                    q.add(i);
+                    queue.add(i);
+                    // System.out.println(Arrays.toString(visit));
                 }
             }
         }
         
     }
     
-    
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        int visit[]=new int[n];
+        visit=new int[n];
         for(int i=0;i<n;i++){
-            if(visit[i]==0){
-                answer+=1;
-                bfs(visit,computers,i,n);
-            }
+            if(visit[i]==1)
+                continue;
+            bfs(i,n,computers);    
+            answer+=1;
         }
         
         return answer;
     }
-    
-    
-    
-    
 }
