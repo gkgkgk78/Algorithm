cd133882aabaa87cbd3584f4dacff5e92339e2e6
gkgkgk78 <78834028+gkgkgk78@users.noreply.github.com>
2025-08-03 22:28:24 +0900

    [level 2] Title: 타겟 넘버, Time: 58.05 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,41 +1,41 @@
 import java.util.*;
 
 class Solution {
+    
+    static int[]numbers;
+    static int target;
     static int answer=0;
-    //n개의 음이 아닌 정수들이 존재한다
-    //적절히 더하거나 빼서 타겟 넘버를 만들려고 한다
-    public void dfs(int[]numbers,String []go,int target,int last,int index){
-        if(index==last)
-        {
+    
+    public void dfs(char[] array,int now){
+        if(now==numbers.length){
             int temp=0;
             for(int i=0;i<numbers.length;i++){
-                if(go[i]=="-")
-                {
+                if(array[i]=='-'){
                     temp-=numbers[i];
                 }
-                else
+                else{
                     temp+=numbers[i];
+                }
             }
-
-            if(temp==target){
+            if(temp==target)
                 answer+=1;
-        }
-
             return;
         }
-        go[index]="-";
-        dfs(numbers,go,target,last,index+1);
-        go[index]="+";
-        dfs(numbers,go,target,last,index+1);
 
+            array[now]='-';
+            dfs(array,now+1);
+            array[now]='+';
+            dfs(array,now+1);
         
     }
     
     
-    public int solution(int[] numbers, int target) {
-        String[] go=new String[numbers.length];  
-        dfs(numbers,go,target,numbers.length,0);
-        
+    
+    public int solution(int[] number, int targe) {
+        numbers=number;
+        target=targe;
+        char [] array=new char[numbers.length];
+        dfs(array,0);
         return answer;
     }
 }
