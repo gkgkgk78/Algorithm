6573cbbde3a74b4b1958f20a3ce8a0a384e9d943
gkgkgk78 <78834028+gkgkgk78@users.noreply.github.com>
2025-08-04 21:15:04 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.45 ms, Memory: 56 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,63 +1,51 @@
 import java.util.*;
+
 class Solution {
     
     public class go{
-        
-        int x,y,size;
-        public go(int x, int y, int size){
+        int x,y;
+        go(int x, int y){
             this.x=x;
             this.y=y;
-            this.size=size;
         }
-        
-        
     }
     
     
-    public int bfs(int [][]maps){
-     
-        PriorityQueue<go>q=new PriorityQueue<>((x,y)->{
-            return x.size-y.size;
-        });
-        int n=maps.length;
-        int m=maps[0].length;
-        int visit[][]=new int[maps.length][maps[0].length];
-        q.add(new go(0,0,1));
-        visit[0][0]=1;
+    public int bfs(int[][]maps){
         int dx[]={-1,0,1,0};
         int dy[]={0,1,0,-1};
+        int n=maps.length;
+        int m=maps[0].length;
+        int visit[][]=new int[n][m];
+        Deque<go>q=new LinkedList<>();
+        q.add(new go(0,0));
+        visit[0][0]=1;
         while(!q.isEmpty()){
-            go now=q.poll();
-            if(now.x==n-1 && now.y==m-1)
-                continue;
+            go a=q.poll();
+            int now=visit[a.x][a.y];
             for(int i=0;i<4;i++){
-                int zx=dx[i]+now.x;
-                int zy=dy[i]+now.y;
-                if (0<=zx&&zx<n &&0<=zy&&zy<m &&maps[zx][zy]==1){
-                    if(visit[zx][zy]==0){
-                        visit[zx][zy]=now.size+1;
-                        q.add(new go(zx,zy,now.size+1));
-                    }
-                    else{
-                        if(now.size+1<visit[zx][zy]){
-                            visit[zx][zy]=now.size+1;
-                            q.add(new go(zx,zy,now.size+1));
-                        }
-                    }
+                int zx=dx[i]+a.x;
+                int zy=dy[i]+a.y;
+                if(0<=zx&&zx<n &&0<=zy&&zy<m){
+                    if (visit[zx][zy]!=0 || maps[zx][zy]==0)continue;
+                    visit[zx][zy]=now+1;
+                    if(zx==n-1 &&zy==m-1)
+                        return now+1;
+                    q.add(new go(zx,zy));
                 }
             }
-            
         }
-        if(visit[n-1][m-1]==0)
-            return -1;
-        return visit[n-1][m-1];
+        
+        return -1;
     }
-    
     
     
     public int solution(int[][] maps) {
         int answer = 0;
-        return bfs(maps);
+        //상대팀 진영에 최대한 빨리 도착 하도록 하
+        
+        answer=bfs(maps);
+        
+        return answer;
     }
-    
 }
