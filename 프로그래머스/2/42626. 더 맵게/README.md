a6761d142d0ce9f2a56baf2f7358ac86bace26d0
gkgkgk78 <78834028+gkgkgk78@users.noreply.github.com>
2025-09-25 16:46:57 +0900

    [level 2] Title: 더 맵게, Time: 1772.80 ms, Memory: 69.5 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/README.md
+++ 프로그래머스/2/42626. 더 맵게/README.md
@@ -1,10 +1,10 @@
 # [level 2] 더 맵게 - 42626 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626?language=python3) 
 
 ### 성능 요약
 
-메모리: 129 MB, 시간: 1423.90 ms
+메모리: 69.5 MB, 시간: 1772.80 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 05일 23:19:22
+2025년 09월 25일 16:46:56
 
 ### 문제 설명
 
--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -1,25 +1,18 @@
 import heapq
 def solution(scoville, K):
     answer = 0
-    #매운것을 좋아하는 leo는 모든 음식의 스코빌 지수를 k이상으로 만들고 싶어 한다
-    #섞어야 하는 최소 횟수를 return 하도록 하라
     q=[]
-    check=0
     for i in scoville:
         heapq.heappush(q,i)
+    temp=0
     while q:
-        a1=heapq.heappop(q)
-        if a1>=K:
-            check=1
-            break
+        now=heapq.heappop(q)
+        if now>=K:
+            return temp
         if len(q)==0:
             break
-        a2=heapq.heappop(q)
-
-        heapq.heappush(q,a1+a2*2)
-        answer+=1
-    if check==1:
-        return answer
-      
-    
+        now1=heapq.heappop(q)
+        heapq.heappush(q,now+now1*2)
+        temp+=1
     return -1
+    
