07acdad587ec28aa6d7f485990497ff0b051bef8
gkgkgk78 <78834028+gkgkgk78@users.noreply.github.com>
2025-10-09 14:44:35 +0900

    [level 2] Title: 더 맵게, Time: 1589.60 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/README.md
+++ 프로그래머스/2/42626. 더 맵게/README.md
@@ -1,10 +1,10 @@
 # [level 2] 더 맵게 - 42626 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626?language=java) 
 
 ### 성능 요약
 
-메모리: 69.5 MB, 시간: 1772.80 ms
+메모리: 124 MB, 시간: 1589.60 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 25일 16:46:56
+2025년 10월 09일 14:44:33
 
 ### 문제 설명
 
--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -1,27 +1,26 @@
 import java.util.*;
+
 class Solution {
-    
-    public int solution(int[] scoville, int k) {
+    public int solution(int[] scoville, int K) {
         int answer = 0;
-        //모든 음식의 스코빌 지수를 k이상으로 만들고자 한다.
-        PriorityQueue<Integer>q=new PriorityQueue<>((a,b)->a-b);
-        
-        for(int i=0;i<scoville.length;i++){
-            q.add(scoville[i]);
+        PriorityQueue<Integer>q=new PriorityQueue<>();
+        for(int i:scoville){
+            q.add(i);
         }
+        
         while(!q.isEmpty()){
-            int a1=q.poll();
-            // System.out.println(a1);
-            if(a1>=k){
+            int now=q.poll();
+            if(now>=K)
                 return answer;
-            }
             if(q.isEmpty())
                 return -1;
-            int a2=q.poll();
-            // System.out.println(a2);
-            q.add(a1+a2*2);
+            int now1=q.poll();
+            q.add(now+now1*2);
             answer+=1;
+
+        
         }
+
         return -1;
     }
 }
