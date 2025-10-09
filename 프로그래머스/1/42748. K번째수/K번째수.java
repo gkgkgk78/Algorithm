5a1d9f7a83207264a7a6d7de301c9b5ded1361d1
gkgkgk78 <78834028+gkgkgk78@users.noreply.github.com>
2025-10-09 15:04:06 +0900

    [level 1] Title: K번째수, Time: 0.55 ms, Memory: 88.2 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -4,18 +4,17 @@ class Solution {
         int[] answer = new int[commands.length];
         
         for(int i=0;i<commands.length;i++){
-            int [] temp=commands[i];
-            List<Integer>list=new LinkedList<>();
-            for (int j=temp[0]-1;j<temp[1];j++){
-                list.add(array[j]);
+            int []temp=commands[i];
+            int first=temp[0];
+            int second=temp[1];
+            int third=temp[2];
+            List<Integer>last=new ArrayList<>();
+            for(int i1=first-1;i1<second;i1++){
+                last.add(array[i1]);
             }
-            list.sort((x,y)->
-                      {
-                          return x-y;
-                      });
-            answer[i]=list.get(temp[2]-1);
+            last.sort((x,y)->{return x-y;});
+            answer[i]=last.get(third-1);       
         }
-        
         
         
         
--- 프로그래머스/1/42748. K번째수/README.md
+++ 프로그래머스/1/42748. K번째수/README.md
@@ -1,10 +1,10 @@
 # [level 1] K번째수 - 42748 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748) 
 
 ### 성능 요약
 
-메모리: 77.7 MB, 시간: 0.95 ms
+메모리: 88.2 MB, 시간: 0.55 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 19일 22:49:32
+2025년 10월 09일 15:04:03
 
 ### 문제 설명
 
