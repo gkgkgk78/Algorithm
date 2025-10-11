982772056adbb6bb51a9f573f59c484e4b60eeef
gkgkgk78 <78834028+gkgkgk78@users.noreply.github.com>
2025-10-11 21:21:58 +0900

    [level 2] Title: 소수 찾기, Time: 30.99 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,72 +1,68 @@
 import java.util.*;
+
 class Solution {
-    static int answer=0;
-    static HashMap<Integer,Integer> map=new HashMap<>();
-    public int check(int now)
-    {
+    
+    static Set<Integer>list=new HashSet<>();
+    static int[]visit;
+    static int[]isSelected;
+
+
+    public void perm(String number,int cnt,int last){
+    
+        if (cnt==last){
+            
+            String temp="";
+            for(int i=0;i<isSelected.length;i++){
+                temp+=number.charAt(isSelected[i]);
+            }
+            int now=Integer.parseInt(temp);
+            list.add(now);
+            return;
+        }
+        
+        for(int i=0;i<number.length();i++){
+            if(visit[i]==1)
+                continue;
+            visit[i]=1;
+            isSelected[cnt]=i;
+            perm(number,cnt+1,last);
+            visit[i]=0;
+        }
+    }
+    
+    
+    public int check(int now){
+        
         if(now<2)
             return 0;
-        for(int i=2;i<(int)Math.sqrt(now)+1;i++){
+        if(now==2)
+            return 1;
+        for(int i=2;i<=Math.sqrt(now);i++){
             if(now%i==0)
                 return 0;
         }
-    
-        return 1;
-    }
-    
-    
-    
-    public void perm(int cnt, int [] isvisit,int [] isselected,char[]temp)
-    {  
-        if(cnt==isselected.length)
-        {
-            String aa="";
-            for(int i=0;i<isselected.length;i++){
-                //System.out.print(isselected[i]);
-                aa+=String.valueOf(temp[isselected[i]]);
-            }
-            //System.out.println(aa);
-            if(map.containsKey(Integer.valueOf(aa)))
-            {
-                return;
-            }
-            
-            int a1=check(Integer.valueOf(aa));
-            
-            //소수 판별 하기
-            if (a1==1){
-                answer+=1;
-                map.put(Integer.valueOf(aa),1);
-                System.out.println(Integer.valueOf(aa));
-            }
-            return;
-        }
-        for(int i=0;i<isvisit.length;i++)
-        {
-            if(isvisit[i]==0)
-            {
-                isvisit[i]=1;
-                isselected[cnt]=i;
-                perm(cnt+1,isvisit,isselected,temp);
-                isvisit[i]=0;
-            }
-        }
+        return 1;        
         
         
     }
-    
     
     
     public int solution(String numbers) {
+        int answer = 0;
         
-        char [] temp=numbers.toCharArray();
-        //완전 탐색을 진행 하게 될건데
-        for(int i=1;i<=temp.length;i++)
-        {
-            int []isvisit=new int[temp.length];
-            int [] isselected=new int[i];
-            perm(0,isvisit,isselected,temp);
+        for(int i=1;i<=numbers.length();i++){
+            //1부터 n까지 순열 조합 구하기
+            visit=new int[numbers.length()];
+            isSelected=new int[i];
+            perm(numbers,0,i);
+                
         }
+        
+        for(int i: list){
+            answer+=check(i);
+        }
+        
+        
         
         
         
