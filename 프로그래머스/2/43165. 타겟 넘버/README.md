cac263de003dad1894fc4c3eb8c9151ad811584f
gkgkgk78 <78834028+gkgkgk78@users.noreply.github.com>
2025-10-12 20:53:56 +0900

    [level 2] Title: 타겟 넘버, Time: 56.90 ms, Memory: 95 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=java) 
 
 ### 성능 요약
 
-메모리: 9.15 MB, 시간: 2499.05 ms
+메모리: 95 MB, 시간: 56.90 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 23일 22:28:26
+2025년 10월 12일 20:53:52
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,41 +1,37 @@
 import java.util.*;
-
 class Solution {
     
-    static int[]numbers;
-    static int target;
+    static int [] array;
+    
     static int answer=0;
     
-    public void dfs(char[] array,int now){
-        if(now==numbers.length){
+    public void dfs(int depth, int [] numbers,int target){
+        if(depth==numbers.length){
             int temp=0;
             for(int i=0;i<numbers.length;i++){
-                if(array[i]=='-'){
-                    temp-=numbers[i];
+                if(array[i]==1){
+                    temp+=numbers[i];
                 }
                 else{
-                    temp+=numbers[i];
+                    temp-=numbers[i];
                 }
             }
             if(temp==target)
                 answer+=1;
             return;
         }
-
-            array[now]='-';
-            dfs(array,now+1);
-            array[now]='+';
-            dfs(array,now+1);
-        
+        array[depth]=1;
+        dfs(depth+1,numbers,target);
+        array[depth]=-1;
+        dfs(depth+1,numbers,target);
+    
     }
     
-    
-    
-    public int solution(int[] number, int targe) {
-        numbers=number;
-        target=targe;
-        char [] array=new char[numbers.length];
-        dfs(array,0);
+     
+    public int solution(int[] numbers, int target) {
+        array=new int[numbers.length];
+        dfs(0,numbers,target);
         return answer;
     }
+    
 }
