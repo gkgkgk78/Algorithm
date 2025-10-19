95799daa07c7f94062c36d5b4493bfd3dc3f464a
gkgkgk78 <78834028+gkgkgk78@users.noreply.github.com>
2025-10-19 21:03:38 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 516 ms, Memory: 62072 KB -BaekjoonHub

--- 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
@@ -1,146 +1,92 @@
+
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.sql.SQLOutput;
+import java.util.*;
 
 public class Main {
 
-	static int tx[] = { -2, -2, -1, -1, 1, 1, 2, 2 };
-	static int ty[] = { -1, 1, -2, 2, -2, 2, -1, 1 };
-	static int k;
-	static int n, m;
-	static int visit[][][];// 좌표 2개+걸어서 , 말타고온 횟수
-	static int graph[][];
-	static int result = Integer.MAX_VALUE;
+    static int n, m, k;
+    static int graph[][];
+    static boolean[][][] visit;
 
-	static class go {
+    static class go {
+        int x, y, count, dis;
 
-		int x, y, run, horse, flag;// 총 걸은 횟수, 말타고온 횟수
+        go(int x, int y, int count, int dis) {
+            this.x = x;
+            this.y = y;
+            this.count = count;
+            this.dis = dis;
+        }
+    }
 
-		public go(int x, int y, int run, int horse, int flag) {
-			this.x = x;
-			this.y = y;
-			this.run = run;
-			this.horse = horse;
-			this.flag = flag;
-		}
+    static int bfs() {
 
-	}
+        int[] tx = {-2, -2, -1, -1, 1, 1, 2, 2};
+        int[] ty = {-1, 1, -2, 2, -2, 2, -1, 1};
+        int dx[] = {-1, 0, 1, 0};
+        int dy[] = {0, -1, 0, 1};
+        visit[0][0][0] = true;
+        Deque<go> q = new ArrayDeque<>();
+        q.add(new go(0, 0, 0, 1));
+        while (!q.isEmpty()) {
+            go now = q.poll();
+            if (now.x == n - 1 && now.y == m - 1) {
+                return now.dis - 1;
+            }
+            if (now.count + 1 <= k) {
+                for (int i = 0; i < tx.length; i++) {
+                    int nx = now.x + tx[i];
+                    int ny = now.y + ty[i];
+                    if (0 <= nx && nx < n && 0 <= ny && ny < m && graph[nx][ny] == 0) {
+                        if (!visit[nx][ny][now.count + 1]) {
+                            visit[nx][ny][now.count + 1] = true;
+                            q.add(new go(nx, ny, now.count + 1, now.dis + 1));
+                        }
+                    }
+                }
+            }
+            for (int i = 0; i < dx.length; i++) {
+                int zx = now.x + dx[i];
+                int zy = now.y + dy[i];
+                if (zx >= 0 && zx < n && zy >= 0 && zy < m) {
+                    if (graph[zx][zy] == 0) {
+                        if (!visit[zx][zy][now.count]) {
+                            visit[zx][zy][now.count] = true;
+                            q.add(new go(zx, zy, now.count, now.dis + 1));
+                        }
+                    }
+                }
 
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
+            }
 
-		// 방문처리를 걸어서 온것과 말타고 온것을 구분하여 작성을 해보자
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 
-		k = Integer.parseInt(in.readLine());
-		StringTokenizer s = new StringTokenizer(in.readLine(), " ");
+        }
+        return -1;
+    }
 
-		m = Integer.parseInt(s.nextToken());
-		n = Integer.parseInt(s.nextToken());
 
-		graph = new int[n][m];
-		visit = new int[n][m][k + 1];
-		for (int i = 0; i < n; i++) {
-			s = new StringTokenizer(in.readLine(), " ");
-			for (int j = 0; j < m; j++) {
-				graph[i][j] = Integer.parseInt(s.nextToken());
-				for (int l = 1; l <= k; l++) {
-					visit[i][j][l] = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        k = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine(), " ");
+        m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        graph = new int[n][m];
+        visit = new boolean[n][m][k + 1];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                graph[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        System.out.println(bfs());
 
-				}
-			}
-		}
-		visit[0][0][0]=1;
-		bfs(0, 0);
-		
-		if (result != Integer.MAX_VALUE) {
-			
-				System.out.println(result);
-		}
-		else
-			if(n==1&&m==1)
-				System.out.println(0);
-			else	
-			System.out.println(-1);
-	}
 
-	private static void bfs(int i1, int j1) {
-		// TODO Auto-generated method stub
-
-		Queue<go> q = new LinkedList<>();
-
-		q.add(new go(0, 0, 0, 0, 0));
-
-		int dx[] = { -1, 0, 1, 0 };
-		int dy[] = { 0, 1, 0, -1 };
-		/*
-		 * this.x = x; this.y = y; this.run = run; this.horse = horse; this.flag = flag;
-		 * 
-		 */
-
-		while (!q.isEmpty()) {
-
-			// 처음에 뚜벅이 였다면
-			go temp = q.poll();
-			int x, y, run, horse, flag;
-			x = temp.x;
-			y = temp.y;
-			run = temp.run;
-			horse = temp.horse;
-			flag = temp.flag;
-			int jo = 0;
-			// 뚜벅이로 이동을 함
-			
-			
-			for (int i = 0; i < 4; i++) {
-				int zx = x + dx[i];
-				int zy = y + dy[i];
-
-				if (0 <= zx && zx < n && 0 <= zy && zy < m && graph[zx][zy] == 0 && visit[zx][zy][horse] == 0) {
-
-					visit[zx][zy][horse] =1;
-					q.add(new go(zx, zy, run + 1, horse, 0));
-
-					if (zx == n - 1 && zy == m - 1) {
-						jo =run+horse+1;
-						if (jo < result)
-							result = jo;
-						return;
-					}
-
-				}
-
-			}
-
-			/// 걸어서 이동을 함
-			if (horse < k) {
-				for (int i = 0; i < 8; i++) {
-					int zx = x + tx[i];
-					int zy = y + ty[i];
-
-					if (0 <= zx && zx < n && 0 <= zy && zy < m && graph[zx][zy] == 0 && visit[zx][zy][horse+1] == 0) {
-
-						visit[zx][zy][horse+1] =1;
-						q.add(new go(zx, zy, run, horse + 1, 0));
-
-						if (zx == n - 1 && zy == m - 1) {
-							jo =run+horse+1;
-							if (jo < result)
-								result = jo;
-							return;
-						}
-
-					}
-
-				}
-
-			}
-
-		}
-
-	}
+    }
 
 }
